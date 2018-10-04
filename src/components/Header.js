57e55e5c5b728128683b54066c8926d9974d9d72
김정근 <snorlax@ridi.com>
2018-10-04 18:48:52 +0900

    Remove prefetch

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,10 +7,10 @@ const linkStyle = {
 
 const Header = () => (
   <div>
-    <Link prefetch href="/">
+    <Link href="/">
       <a style={linkStyle}>Home</a>
     </Link>
-    <Link prefetch href="/about">
+    <Link href="/about">
       <a style={linkStyle}>About</a>
     </Link>
   </div>
