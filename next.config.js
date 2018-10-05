d2120989feed3ef038951cb5555323fe8ac7c712
김정근 <snorlax@ridi.com>
2018-10-05 16:34:23 +0900

    Addstaging environment

--- next.config.js
+++ next.config.js
@@ -1,11 +1,13 @@
 
 const DEVELOPMENT = 'development';
+const STAGING = 'staging';
 const PRODUCTION = 'production';
 
 const getStaticUrl = environment => {
   switch (environment) {
     case DEVELOPMENT:
       return 'https://library-static.dev.ridi.io';
+    case STAGING:
     case PRODUCTION:
       return 'https://library-static.ridibooks.com';
     default:
