2d5f670e7fb27356b20a58666e58be3311296b6d
김정근 <snorlax@ridi.com>
2018-10-08 16:04:04 +0900

    rollback cluster name

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -18,8 +18,7 @@ run_and_check_exit_code() {
     done
 }
 
-run_and_check_exit_code "ecs-cli compose --cluster library-a-cluster --project-name library -f docker/compose/web.yml service up" \
-                        "ecs-cli compose --cluster library-c-cluster --project-name library -f docker/compose/web.yml service up"
+run_and_check_exit_code "ecs-cli compose --cluster library-cluster --project-name library -f docker/compose/web.yml service up"
 
 if [ $? -ne 0 ]; then
     echo "deploy fails"
