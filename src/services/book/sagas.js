d08e63189cdd1403d227a2c6f5f81e8440edf9de
김정근 <snorlax@ridi.com>
2018-10-22 17:34:49 +0900

    Connect storage

--- src/services/book/sagas.js
+++ src/services/book/sagas.js
@@ -1,18 +1,22 @@
 
-import { all, call, put, select, takeEvery } from 'redux-saga/effects';
+import { all, call, put, select, takeEvery, take, cancel, fork } from 'redux-saga/effects';
+import { delay } from 'redux-saga/utils';
 
 import {
   LOAD_BOOK_DATA,
-  LOAD_BOOK_DATA_FROM_STORAGE
+  LOAD_BOOK_DATA_FROM_STORAGE,
+  START_PERSIST_TIMER,
+  STOP_PERSIST_TIMER,
 } from './actions';
 
 import { fetchBookData } from './requests';
+
+import Storage from '../../utils/storage';
 
 
 function* loadBookData (action) {
   // Step 1. Fetch book data
   // Step 2. Set book data
-
   const books = yield call(fetchBookData, action.payload.bookIds);
   yield put(setBookData(books));
 }
@@ -21,18 +25,34 @@ function* loadBookData (action) {
 function* loadBookDataFromStorage () {
   // Step 1. Load book data from storage
   // Step 2. Set book data
+  const books = Storage.load();
+  yield put(setBookData(books));
 }
 
 
-function* watchPersistTimer () {
+function* timerTick () {
+  const PERSIST_DELAY = 1000 * 30;
+
+  while (true) {
+    yield call(delay, PERSIST_DELAY);
+    const books = yield select(state => state.books.books);
+    Storage.save(books);
+  }
+}
+
+export function* persistTimer () {
   // Step 1. Select book data in redux store.
   // Step 2. Save to storage.
-
-  const books = yield select(state => state.books);
+  while(yield take(START_PERSIST_TIMER)) {
+    const timer = yield fork(timerTick);
+    yield take(STOP_PERSIST_TIMER);
+    yield cancel(timer);
+  }
 }
 
 export default function* bookRootSaga () {
   yield all([
+    persistTimer(),
     takeEvery(LOAD_BOOK_DATA, loadBookData),
     takeEvery(LOAD_BOOK_DATA_FROM_STORAGE, loadBookDataFromStorage),
   ]);
--- src/store/bootstrap.js
+++ src/store/bootstrap.js
@@ -1,12 +1,14 @@
 
 import { loadUserInfo } from '../services/account/actions';
 
-import { loadBookData } from '../services/book/actions';
+import { loadBookData, startPersistTimer } from '../services/book/actions';
 
 
 const bootstrap = async store => {
   await store.dispatch(loadUserInfo());
+  await store.dispatch(startPersistTimer());
 
+  // await store.dispatch(loadBookDataFromStorage());
   // Temp
   await store.dispatch(loadBookData(['2066000162', '745000083']));
 };
