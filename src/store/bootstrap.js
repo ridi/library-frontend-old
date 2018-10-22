d9e20a32059c9a3ba86b612fbcb42fbe21402307
김정근 <snorlax@ridi.com>
2018-10-22 19:29:24 +0900

    Remove temporary code

--- src/store/bootstrap.js
+++ src/store/bootstrap.js
@@ -1,15 +1,13 @@
 
 import { loadUserInfo } from '../services/account/actions';
 
-import { loadBookData, loadBookDataFromStorage, startPersistTimer } from '../services/book/actions';
+import { loadBookDataFromStorage, startPersistTimer } from '../services/book/actions';
 
 
 const bootstrap = async store => {
   await store.dispatch(loadUserInfo());
   await store.dispatch(startPersistTimer());
   await store.dispatch(loadBookDataFromStorage());
-  // Temp
-  await store.dispatch(loadBookData(['2066000162', '745000083']));
 };
 
 
