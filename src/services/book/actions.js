c2e644b77a9a00682ad1c7fc3ceb41f2adb788f4
김정근 <snorlax@ridi.com>
2018-10-22 14:07:17 +0900

    Add reducer and actions for saving, persisting book data

--- src/services/book/actions.js
+++ src/services/book/actions.js
@@ -1,3 +1,14 @@
 
-
 export const SAVE_BOOK_DATA = 'SAVE_BOOK_DATA';
+export const PERSIST_BOOK_DATA = 'PERSIST_BOOK_DATA';
+
+export const saveBookData = books => ({
+  type: SAVE_BOOK_DATA,
+  payload: {
+    books,
+  }
+});
+
+export const persistBookData = () => ({
+  type: PERSIST_BOOK_DATA,
+});
--- src/services/book/reducers.js
+++ src/services/book/reducers.js
@@ -1,9 +1,12 @@
 
 import initialState from './state';
+import { SAVE_BOOK_DATA } from './actions';
 
 
 const BookReducer = (state = initialState, action) => {
   switch(action.type) {
+    case SAVE_BOOK_DATA:
+      pass
     default:
       return state
   }
