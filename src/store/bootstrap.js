2099793162682f3557cb2562ff7dfaa0f1460926
김정근 <snorlax@ridi.com>
2018-10-23 12:35:48 +0900

    Remove Timer in bootstrap

--- src/store/bootstrap.js
+++ src/store/bootstrap.js
@@ -1,12 +1,11 @@
 
 import { loadUserInfo } from '../services/account/actions';
 
-import { loadBookDataFromStorage, startPersistTimer } from '../services/book/actions';
+import { loadBookDataFromStorage } from '../services/book/actions';
 
 
 const bootstrap = async store => {
   await store.dispatch(loadUserInfo());
-  await store.dispatch(startPersistTimer());
   await store.dispatch(loadBookDataFromStorage());
 };
 
