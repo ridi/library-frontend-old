edad8a41d2f75067d39624ca8a6a7e20e0752718
김정근 <snorlax@ridi.com>
2018-10-23 21:49:27 +0900

    Be more safety Storage

--- src/utils/storage.js
+++ src/utils/storage.js
@@ -3,14 +3,22 @@ const LOCAL_STORAGE_KEY = 'library.books';
 
 export default {
   load: () => {
+    if (typeof localStorage === 'undefined' || localStorage === null) {
+      return [];
+    }
+
     const data = localStorage.getItem(LOCAL_STORAGE_KEY);
     if (!data) {
-      return {};
+      return [];
     }
 
     return JSON.parse(data);
   },
   save: state => {
+    if (typeof localStorage === 'undefined' || localStorage === null) {
+      return;
+    }
+
     try {
       localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(state));
     }  catch(e) {
