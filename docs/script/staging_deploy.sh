56129e0dd7950622aec1d42dac4b62aebeeb1933
김정근 <snorlax@ridi.com>
2018-10-24 11:40:26 +0900

    Change slack message

--- docs/script/staging_deploy.sh
+++ docs/script/staging_deploy.sh
@@ -26,4 +26,4 @@ if [ $? -ne 0 ]; then
 fi
 
 
-curl -X POST --data-urlencode "payload={\"text\": \"[$ENVIRONMENT - $TAG] 내 서재 배포가 완료되었습니다!!!!!!!\nRepo: https://gitlab.com/ridicorp/account/library-web\"}" $SLACK_DEPLOY_HOOK
+curl -X POST --data-urlencode "payload={\"text\": \"[내서재 웹][$ENVIRONMENT - $TAG] 배포가 완료되었습니다.\nRepo: https://gitlab.com/ridicorp/account/library-web\"}" $SLACK_DEPLOY_HOOK
