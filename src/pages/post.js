b82781edd37eb355d55b39d84a5e023f26a02931
김정근 <snorlax@ridi.com>
2018-10-25 18:24:59 +0900

    Fix lint

--- src/pages/post.js
+++ src/pages/post.js
@@ -1,15 +1,15 @@
 import fetch from 'isomorphic-unfetch';
 import Layout from '../components/Layout';
 
-const Post = props => (
+const Post = ({ show }) => (
   <Layout>
-    <h1>{props.show.name}</h1>
-    <p>{props.show.summary.replace(/<[/]?p>/g, '')}</p>
-    <img src={props.show.image.medium} />
+    <h1>{show.name}</h1>
+    <p>{show.summary.replace(/<[/]?p>/g, '')}</p>
+    <img src={show.image.medium} alt="" />
   </Layout>
 );
 
-Post.getInitialProps = async function(context) {
+Post.getInitialProps = async context => {
   const { id } = context.query;
   const res = await fetch(`https://api.tvmaze.com/shows/${id}`);
   const show = await res.json();
