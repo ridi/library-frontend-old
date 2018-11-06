1aa438e978a5ccb6672358c1d0a9fbc62da5b941
김정근 <snorlax@ridi.com>
2018-11-06 12:15:09 +0900

    Option 가져오는 Select적용

--- src/services/search/selectors.js
+++ src/services/search/selectors.js
@@ -6,3 +6,18 @@ export const getPageInfo = createSelector(getSearchState, searchState => ({
   page: searchState.page,
   keyword: searchState.keyword,
 }));
+
+export const getPage = createSelector(
+  getSearchState,
+  searchState => searchState.page,
+);
+
+export const getKeyword = createSelector(
+  getSearchState,
+  searchState => searchState.keyword,
+);
+
+export const getSearchOptions = createSelector(
+  [getPage, getKeyword],
+  (page, keyword) => ({ page, keyword }),
+);
