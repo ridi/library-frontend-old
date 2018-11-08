5b3acd05df59ecf85a85d3f029d6138e5867bc1d
nnyamm <nnyamm@ridi.com>
2018-11-08 21:32:38 +0900

    Fix snakelize

--- src/utils/snakelize.js
+++ src/utils/snakelize.js
@@ -1,4 +1,4 @@
-const _camelToSnake = name => name.replace(/([A-Z])/g, (x, y) => `_${y.toLowerCase()}`.replace(/^_/, ''));
+const _camelToSnake = name => name.replace(/([A-Z])/g, (x, y) => `_${y.toLowerCase()}`).replace(/^_/, '');
 
 export const snakelize = dict =>
   Object.keys(dict).reduce((previous, key) => {
