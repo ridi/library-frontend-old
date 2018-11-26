193978ba0fd24947f1b79ae0b7c1140fb9837571
김정근 <snorlax@ridi.com>
2018-11-26 16:46:52 +0900

    login 대신 ridi/authorize로 이동

--- server/config.js
+++ server/config.js
@@ -1,4 +1,5 @@
 module.exports = {
-  LOGIN_URL: process.env.LOGIN_URL || 'https://account.dev.ridi.io/accounts/login',
+  LOGIN_URL: process.env.LOGIN_URL || 'https://account.dev.ridi.io/ridi/authorize',
+  CLIENT_ID: process.env.CLIENT_ID || 'Nkt2Xdc0zMuWmye6MSkYgqCh9q6JjeMCsUiH1kgL',
   JWT_SECRET: process.env.JWT_SECRET || '***REMOVED***',
 };
