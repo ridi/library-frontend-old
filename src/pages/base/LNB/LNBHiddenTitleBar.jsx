3f697125745f67c68c82b52222ea7e8214df2bb6
김정근 <snorlax@ridi.com>
2018-11-27 22:00:44 +0900

    Style Component통합

--- src/pages/base/LNB/LNBHiddenTitleBar.jsx
+++ src/pages/base/LNB/LNBHiddenTitleBar.jsx
@@ -1,25 +1,91 @@
 import React from 'react';
 import Link from 'next/link';
+import { css } from 'emotion';
 import { Icon } from '@ridi/rsg';
-import * as style from './styles';
 import Responsive from '../Responsive';
 import IconButton from '../../../components/IconButton';
 
+const styles = {
+  LNBHiddenTitleBarWrapper: css({
+    height: 46,
+    backgroundColor: '#ffffff',
+    boxShadow: '0 2px 10px 0 rgba(0, 0, 0, 0.04)',
+    boxSizing: 'border-box',
+    borderBottom: '1px solid #d1d5d9',
+  }),
+
+  LNBHiddenTitleBar: css({
+    display: 'flex',
+    alignItems: 'center',
+    height: 46,
+  }),
+
+  LNBHiddenTitleBarBackIconWrapper: css({
+    padding: '5px 10px 5px 0',
+    height: 20,
+  }),
+  LNBHiddenTitleBarBackIcon: css({
+    width: 20,
+    height: 20,
+    '.RSGIcon': {
+      width: 16,
+      height: 16,
+    },
+  }),
+
+  LNBHiddenTitleBarTitleWrapper: css({
+    height: 30,
+    width: '100%',
+    display: 'flex',
+    alignItems: 'center',
+  }),
+  LNBHiddenTitleBarTitle: css({
+    fontSize: 16,
+    fontWeight: 'bold',
+    letterSpacing: -0.3,
+    color: '#40474d',
+  }),
+
+  LNBHiddenTitleBarHiddenCount: css({
+    paddingLeft: 6,
+    fontSize: 15,
+    fontweight: 'bold',
+    letterSpacing: -0.3,
+    color: '#808991',
+  }),
+
+  LNBHiddenTitleBarToolsWrapper: css({}),
+  LNBHiddenTitleBarTool: css({
+    margin: '3px 0',
+    width: 24,
+    height: 24,
+    marginRight: 16,
+    '&:last-of-type': {
+      marginRight: 0,
+    },
+    '.RSGIcon': {
+      width: 20,
+      height: 20,
+      fill: '#40474d',
+    },
+  }),
+};
+
 const LNBHiddenTitleBar = ({ title, hiddenTotalCount, href, as, a11y = '뒤로가기', onClickEditingMode }) => (
-  <nav className={style.LNBTitleBarWrapper}>
-    <Responsive className={style.LNBTitleBar}>
+  <nav className={styles.LNBHiddenTitleBarWrapper}>
+    <Responsive className={styles.LNBHiddenTitleBar}>
       <Link href={href} as={as}>
-        <a className={style.LNBTitleBarBackIconWrapper}>
-          <Icon name="arrow_3_left" className={style.LNBTitleBarBackIcon} />
+        <a className={styles.LNBHiddenTitleBarBackIconWrapper}>
+          <Icon name="arrow_3_left" className={styles.LNBHiddenTitleBarBackIcon} />
           <span className="a11y">{a11y}</span>
         </a>
       </Link>
-      <div className={style.LNBTitleBarTitleWrapper}>
-        <span className={style.LNBTitleBarTitle}>{title}</span>
-        <span className={style.LNBHiddenTitleBarHiddenCount}>{hiddenTotalCount}</span>
+      <div className={styles.LNBHiddenTitleBarTitleWrapper}>
+        <span className={styles.LNBHiddenTitleBarTitle}>{title}</span>
+        <span className={styles.LNBHiddenTitleBarHiddenCount}>{hiddenTotalCount}</span>
       </div>
-      <div className={style.LNBHiddenTitleBarToolsWrapper}>
-        <IconButton icon="check_3" a11y="편집" className={style.LNBHiddenTitleBarTool} onClick={onClickEditingMode} />
+      <div className={styles.LNBHiddenTitleBarToolsWrapper}>
+        <IconButton icon="check_3" a11y="편집" className={styles.LNBHiddenTitleBarTool} onClick={onClickEditingMode} />
       </div>
     </Responsive>
   </nav>
