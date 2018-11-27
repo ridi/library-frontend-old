b84fa4f29bf4b5e8d31a3ecdc5be981059e55f95
김정근 <snorlax@ridi.com>
2018-11-27 21:52:35 +0900

    import 이름 변경

--- src/pages/base/LNB/LNBHiddenTitleBar.jsx
+++ src/pages/base/LNB/LNBHiddenTitleBar.jsx
@@ -1,25 +1,25 @@
 import React from 'react';
 import Link from 'next/link';
 import { Icon } from '@ridi/rsg';
-import * as styles from './styles';
+import * as style from './styles';
 import Responsive from '../Responsive';
 import IconButton from '../../../components/IconButton';
 
 const LNBHiddenTitleBar = ({ title, hiddenTotalCount, href, as, a11y = '뒤로가기', onClickEditingMode }) => (
-  <nav className={styles.LNBTitleBarWrapper}>
-    <Responsive className={styles.LNBTitleBar}>
+  <nav className={style.LNBTitleBarWrapper}>
+    <Responsive className={style.LNBTitleBar}>
       <Link href={href} as={as}>
-        <a className={styles.LNBTitleBarBackIconWrapper}>
-          <Icon name="arrow_3_left" className={styles.LNBTitleBarBackIcon} />
+        <a className={style.LNBTitleBarBackIconWrapper}>
+          <Icon name="arrow_3_left" className={style.LNBTitleBarBackIcon} />
           <span className="a11y">{a11y}</span>
         </a>
       </Link>
-      <div className={styles.LNBTitleBarTitleWrapper}>
-        <span className={styles.LNBTitleBarTitle}>{title}</span>
-        <span className={styles.LNBHiddenTitleBarHiddenCount}>{hiddenTotalCount}</span>
+      <div className={style.LNBTitleBarTitleWrapper}>
+        <span className={style.LNBTitleBarTitle}>{title}</span>
+        <span className={style.LNBHiddenTitleBarHiddenCount}>{hiddenTotalCount}</span>
       </div>
-      <div className={styles.LNBHiddenTitleBarToolsWrapper}>
-        <IconButton icon="check_3" a11y="편집" className={styles.LNBHiddenTitleBarTool} onClick={onClickEditingMode} />
+      <div className={style.LNBHiddenTitleBarToolsWrapper}>
+        <IconButton icon="check_3" a11y="편집" className={style.LNBHiddenTitleBarTool} onClick={onClickEditingMode} />
       </div>
     </Responsive>
   </nav>
