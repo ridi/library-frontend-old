a0f3f91b25e247308065bb158b8affc920f8f48e
김정근 <snorlax@ridi.com>
2018-11-27 21:18:06 +0900

    React import 추가

--- src/components/Tabbar/TabBar.jsx
+++ src/components/Tabbar/TabBar.jsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import * as styles from './styles';
 
 const TabBar = ({ children }) => <ul className={styles.TabBar}>{children}</ul>;
--- src/components/Tabbar/TabItem.jsx
+++ src/components/Tabbar/TabItem.jsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import classNames from 'classnames';
 import * as styles from './styles';
 
