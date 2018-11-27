af230bbb81131cf9cda6488b948e5d4c183b3d91
nnyamm <nnyamm@ridi.com>
2018-11-27 20:53:03 +0900

    Add SVGR

--- next.config.js
+++ next.config.js
@@ -25,4 +25,11 @@ module.exports = {
     PLATFORM_API_BASE_URL: process.env.PLATFORM_API_BASE_URL,
     STATIC_URL: process.env.STATIC_URL,
   },
+  webpack: (config, {}) => {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ['@svgr/webpack'],
+    });
+    return config;
+  },
 };
