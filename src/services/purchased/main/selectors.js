1b7d3e4977ac2d497ed7b8cb58eed04dbba1307f
Junheong Park <jh.park@ridi.com>
2018-12-03 18:56:48 +0900

    Code reformat

--- src/services/purchased/main/selectors.js
+++ src/services/purchased/main/selectors.js
@@ -6,46 +6,73 @@ import { MainOrderOptions } from '../../../constants/orderOptions';
 
 const getPurchasedMainState = state => state.purchasedMain;
 
-export const getItems = createSelector(getPurchasedMainState, purchasedMainState => purchasedMainState.items);
+export const getItems = createSelector(
+  getPurchasedMainState,
+  purchasedMainState => purchasedMainState.items,
+);
 
-export const getItemsByPage = createSelector(getPurchasedMainState, purchasedMainState => {
-  const { page, itemIdsForPage, items } = purchasedMainState;
-  const itemIds = itemIdsForPage[page] || [];
-  return itemIds.map(itemId => items[itemId]);
-});
+export const getItemsByPage = createSelector(
+  getPurchasedMainState,
+  purchasedMainState => {
+    const { page, itemIdsForPage, items } = purchasedMainState;
+    const itemIds = itemIdsForPage[page] || [];
+    return itemIds.map(itemId => items[itemId]);
+  },
+);
 
-export const getPageInfo = createSelector(getPurchasedMainState, purchasedMainState => {
-  const {
+export const getPageInfo = createSelector(
+  getPurchasedMainState,
+  purchasedMainState => {
+    const {
+      page,
+      unitTotalCount,
+      order,
+      filter: { selected },
+    } = purchasedMainState;
+
+    const { orderType, orderBy } = MainOrderOptions.parse(order);
+
+    return {
+      currentPage: page,
+      totalPages: calcPage(unitTotalCount, LIBRARY_ITEMS_LIMIT_PER_PAGE),
+      order,
+      orderType,
+      orderBy,
+      filter: selected,
+    };
+  },
+);
+
+export const getPage = createSelector(
+  getPurchasedMainState,
+  purchasedMainState => purchasedMainState.page,
+);
+
+export const getOrder = createSelector(
+  getPurchasedMainState,
+  purchasedMainState => purchasedMainState.order,
+);
+
+export const getFilterOptions = createSelector(
+  getPurchasedMainState,
+  purchasedMainState => purchasedMainState.filter.options,
+);
+
+export const getFilter = createSelector(
+  getPurchasedMainState,
+  purchasedMainState => purchasedMainState.filter.selected,
+);
+
+export const getPurchaseOptions = createSelector(
+  [getPage, getOrder, getFilter],
+  (page, order, filter) => ({
     page,
-    unitTotalCount,
     order,
-    filter: { selected },
-  } = purchasedMainState;
+    filter,
+  }),
+);
 
-  const { orderType, orderBy } = MainOrderOptions.parse(order);
-
-  return {
-    currentPage: page,
-    totalPages: calcPage(unitTotalCount, LIBRARY_ITEMS_LIMIT_PER_PAGE),
-    order,
-    orderType,
-    orderBy,
-    filter: selected,
-  };
-});
-
-export const getPage = createSelector(getPurchasedMainState, purchasedMainState => purchasedMainState.page);
-
-export const getOrder = createSelector(getPurchasedMainState, purchasedMainState => purchasedMainState.order);
-
-export const getFilterOptions = createSelector(getPurchasedMainState, purchasedMainState => purchasedMainState.filter.options);
-
-export const getFilter = createSelector(getPurchasedMainState, purchasedMainState => purchasedMainState.filter.selected);
-
-export const getPurchaseOptions = createSelector([getPage, getOrder, getFilter], (page, order, filter) => ({
-  page,
-  order,
-  filter,
-}));
-
-export const getSelectedBooks = createSelector(getPurchasedMainState, purchasedMainState => purchasedMainState.selectedBooks);
+export const getSelectedBooks = createSelector(
+  getPurchasedMainState,
+  purchasedMainState => purchasedMainState.selectedBooks,
+);
