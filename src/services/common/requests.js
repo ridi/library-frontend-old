d179961d1b801f267c11db62930b5899a9e252d1
김정근 <snorlax@ridi.com>
2018-12-06 14:43:09 +0900

    큐 체크 API 작성

--- src/services/common/requests.js
+++ src/services/common/requests.js
@@ -5,11 +5,45 @@ import { getAPI } from '../../api/actions';
 
 import { makeURI } from '../../utils/uri';
 import { toFlatten } from '../../utils/array';
+import { snakelize } from '../../utils/snakelize';
+import { delay } from '../../utils/delay';
 
 export function* getRevision() {
   const api = yield put(getAPI());
   const response = yield api.get(makeURI('/commands/items/u/revision/', {}, config.LIBRARY_API_BASE_URL));
   return response.data.revision;
+}
+
+export function* requestCheckQueueStatus(queueIds) {
+  const api = yield put(getAPI());
+  let retryCount = 0;
+  const retryMaxCount = 3;
+  const retryDuration = 2000;
+
+  function* _request(_queueIds) {
+    const data = snakelize({
+      queueIds: _queueIds,
+    });
+
+    const response = yield api.post(makeURI('/commands/items/u/queue/status/', {}, config.LIBRARY_API_BASE_URL), data);
+    const { is_finished: isFinished, queue_ids: syncingQueueIds } = response.data;
+
+    if (isFinished) {
+      return true;
+    }
+
+    if (syncingQueueIds.length === 0 || retryCount < retryMaxCount) {
+      return false;
+    }
+
+    retryCount += 1;
+    yield delay(retryDuration);
+    const _isFinished = yield _request(syncingQueueIds);
+    return _isFinished;
+  }
+
+  const isFinish = yield _request(queueIds);
+  return isFinish;
 }
 
 export function* triggerDownload(bookIds) {
---
+++ src/utils/delay.js
@@ -0,0 +1,1 @@
+export const delay = duration => new Promise(resolve => window.setTimeout(resolve, duration));
