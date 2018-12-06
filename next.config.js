576fe2997f59b85d9adbbcf9285b08efe4d6b048
김정근 <snorlax@ridi.com>
2018-12-06 15:09:25 +0900

    next.config.js의 불필요한 메소드 제거

--- next.config.js
+++ next.config.js
@@ -1,24 +1,8 @@
-const DEVELOPMENT = 'development';
-const STAGING = 'staging';
-const PRODUCTION = 'production';
-
-const getStaticUrl = environment => {
-  switch (environment) {
-    case DEVELOPMENT:
-      return 'https://library-static.dev.ridi.io';
-    case STAGING:
-    case PRODUCTION:
-      return 'https://library-static.ridibooks.com';
-    default:
-      return '';
-  }
-};
-
 module.exports = {
   distDir: '../build',
   useFileSystemPublicRoutes: false,
-  exportPathMap: defaultPathMap => ({}),
-  assetPrefix: getStaticUrl(process.env.NODE_ENV),
+  exportPathMap: () => ({}),
+  assetPrefix: process.env.STATIC_URL,
   publicRuntimeConfig: {
     ACCOUNT_BASE_URL: process.env.ACCOUNT_BASE_URL,
     STORE_API_BASE_URL: process.env.STORE_API_BASE_URL,
