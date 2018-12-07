0ff9b88fb1805439d2eb24970bdab06310db5190
김정근 <snorlax@ridi.com>
2018-12-07 15:51:10 +0900

    injectGlobal로 변경

--- src/pages/base/Layout.jsx
+++ src/pages/base/Layout.jsx
@@ -1,13 +1,10 @@
 /** @jsx jsx */
-import { Global, jsx } from '@emotion/core';
 import Footer from './Footer';
 import GNB from './GNB';
-import { reset } from '../../styles/reset';
 import Toaster from '../../components/Toaster';
 
 const Layout = ({ children }) => (
   <>
-    <Global styles={reset} />
     <GNB />
     {children}
     <Footer />
