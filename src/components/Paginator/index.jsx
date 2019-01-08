55fe3b0bbc088cae9239a8386ed6730d9a11320c
nnyamm <nnyamm@ridi.com>
2019-01-08 18:48:28 +0900

    Fix paginator styles

---
+++ src/components/Paginator/index.jsx
@@ -0,0 +1,146 @@
+/** @jsx jsx */
+import React from 'react';
+import Link from 'next/link';
+import { Icon } from '@ridi/rsg';
+import { jsx } from '@emotion/core';
+
+import * as styles from './styles';
+import { calcPageBlock, makePageRange } from '../../utils/pagination';
+import { snakelize } from '../../utils/snakelize';
+
+export default class Paginator extends React.Component {
+  getLinkProps(page) {
+    const { href, as, query = {} } = this.props;
+    const _query = snakelize({
+      ...query,
+      page,
+    });
+
+    return {
+      href: {
+        pathname: href,
+        query: _query,
+      },
+      as: {
+        pathname: as,
+        query: _query,
+      },
+    };
+  }
+
+  renderGoFirst() {
+    // const { currentPage, pageCount } = this.props;
+
+    // // if (currentPage <= pageCount) {
+    // //   return null;
+    // // }
+
+    return (
+      <>
+        <div css={styles.pageItems}>
+          <Link {...this.getLinkProps(1)}>
+            <a css={styles.pageButton}>처음</a>
+          </Link>
+        </div>
+        <span css={styles.paginatorDots}>
+          <Icon name="dotdotdot" css={styles.dots} />
+        </span>
+      </>
+    );
+  }
+
+  renderGoLast() {
+    const { currentPage, totalPages, pageCount } = this.props;
+
+    if (calcPageBlock(currentPage, pageCount) === calcPageBlock(totalPages, pageCount)) {
+      return null;
+    }
+
+    return (
+      <>
+        <span css={styles.paginatorDots}>
+          <Icon name="dotdotdot" css={styles.dots} />
+        </span>
+        <div css={styles.pageItems}>
+          <Link {...this.getLinkProps(totalPages)}>
+            <a css={styles.pageButton}>마지막</a>
+          </Link>
+        </div>
+      </>
+    );
+  }
+
+  renderGoPrev() {
+    const { currentPage } = this.props;
+
+    if (currentPage === 1) {
+      return null;
+    }
+
+    return (
+      <div css={styles.buttonWrapper}>
+        <Link {...this.getLinkProps(currentPage - 1)}>
+          <a css={styles.pageButton}>
+            <Icon name="arrow_8_left" css={styles.pageItemIcon} />
+            <span className="a11y">이전 페이지</span>
+          </a>
+        </Link>
+      </div>
+    );
+  }
+
+  renderGoNext() {
+    const { currentPage, totalPages } = this.props;
+
+    if (currentPage === totalPages) {
+      return null;
+    }
+
+    return (
+      <div css={styles.buttonWrapper}>
+        <Link {...this.getLinkProps(currentPage + 1)}>
+          <a css={styles.pageButton}>
+            <Icon name="arrow_8_right" css={styles.pageItemIcon} />
+            <span className="a11y">다음 페이지</span>
+          </a>
+        </Link>
+      </div>
+    );
+  }
+
+  renderPageItems() {
+    const { currentPage, totalPages, pageCount } = this.props;
+    const pageRange = makePageRange(currentPage, totalPages, pageCount);
+
+    return (
+      <ul css={styles.pageItems}>
+        {pageRange.map(page => (
+          <li key={page} css={styles.pageItem}>
+            <Link {...this.getLinkProps(page)}>
+              <a css={[styles.pageButton, currentPage === page && styles.pageButtonActive]}>{page}</a>
+            </Link>
+          </li>
+        ))}
+      </ul>
+    );
+  }
+
+  render() {
+    const { totalPages } = this.props;
+    if (totalPages === 0) {
+      return null;
+    }
+
+    return (
+      <div css={styles.paginator}>
+        <div css={styles.horizontalWrapper}>
+          {this.renderGoFirst()}
+          {this.renderGoPrev()}
+          {this.renderPageItems()}
+          {this.renderGoNext()}
+          {this.renderGoLast()}
+        </div>
+      </div>
+    );
+  }
+}
---
+++ src/components/Paginator/styles.js
@@ -0,0 +1,76 @@
+const buttonHeight = 30;
+const wrapperStyle = {
+  display: 'inline-block',
+  listStyle: 'none',
+  padding: 0,
+  border: '1px solid #d1d5d9',
+  overflow: 'hidden',
+  borderRadius: '3px',
+  boxShadow: '0 1px 1px 0 rgba(206, 210, 214, 0.3)',
+};
+
+export const paginator = {
+  borderTop: '1px solid #d1d5d9',
+  padding: '9px 0',
+  textAlign: 'center',
+  whiteSpace: 'nowrap',
+};
+
+export const horizontalWrapper = {
+  display: 'inline-flex',
+  alignItems: 'center',
+  justifyContent: 'space-between',
+};
+
+export const pageItems = {
+  ...wrapperStyle,
+};
+
+export const buttonWrapper = {
+  ...wrapperStyle,
+  margin: '0 3px',
+};
+
+export const pageItem = {
+  display: 'inline-block',
+  marginLeft: -1,
+  borderLeft: '1px solid #d1d5d9',
+  verticalAlign: 'top',
+};
+
+export const pageButton = {
+  display: 'block',
+  minWidth: 36,
+  height: buttonHeight,
+  lineHeight: `${buttonHeight}px`,
+  textAlign: 'center',
+  fontSize: 13,
+  color: '#808991',
+};
+
+export const pageButtonActive = {
+  background: '#808991',
+  color: 'white',
+  fontWeight: '700',
+  lineHeight: `${buttonHeight - 2}px`,
+};
+
+export const pageItemIcon = {
+  width: 6,
+  height: 9,
+  fill: '#818a92',
+};
+
+export const paginatorDots = {
+  display: 'inline-block',
+  width: 8,
+  height: buttonHeight + 2,
+  lineHeight: `${buttonHeight - 2}px`,
+  padding: '0 0 0 3px',
+};
+
+export const dots = {
+  width: '100%',
+  verticalAlign: 'middle',
+  fill: '#bfc4c8',
+};
