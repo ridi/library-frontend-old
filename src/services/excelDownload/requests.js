55d0bcbd217010b051f4504b1acaecc7cb10f753
Junheong Park <jh.park@ridi.com>
2019-01-08 20:28:33 +0900

    makeURI 함수를 사용하도록 변경

--- src/services/excelDownload/requests.js
+++ src/services/excelDownload/requests.js
@@ -1,25 +1,24 @@
 import { put } from 'redux-saga/effects';
-import { stringify } from 'qs';
 import { getAPI } from '../../api/actions';
 import config from '../../config';
-import { snakelize } from '../../utils/snakelize';
+import { makeURI } from '../../utils/uri';
 
 export function* fetchStartExcelDownload() {
-  const options = snakelize({
+  const options = {
     t: +new Date(),
-  });
+  };
 
   const api = yield put(getAPI());
-  const response = yield api.post(`${config.LIBRARY_API_BASE_URL}/items/excel?${stringify(options)}`);
+  const response = yield api.post(makeURI('/items/excel', options, config.LIBRARY_API_BASE_URL));
   return response.data;
 }
 
 export function* fetchCheckExcelDownload(queueId) {
-  const options = snakelize({
+  const options = {
     t: +new Date(),
-  });
+  };
 
   const api = yield put(getAPI());
-  const response = yield api.get(`${config.LIBRARY_API_BASE_URL}/items/excel/${queueId}?${stringify(options)}`);
+  const response = yield api.get(makeURI(`items/excel/${queueId}`, options, config.LIBRARY_API_BASE_URL));
   return response.data;
 }
