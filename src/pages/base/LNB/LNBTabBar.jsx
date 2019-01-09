cebce2ea9c7594ce2525080f41a31d139f46bc1b
김정근 <snorlax@ridi.com>
2019-01-09 16:53:36 +0900

    LNBTabBar 롤백

--- src/pages/base/LNB/LNBTabBar.jsx
+++ src/pages/base/LNB/LNBTabBar.jsx
@@ -7,7 +7,6 @@ import { TabBar, TabLinkItem } from '../../../components/TabBar';
 import Responsive from '../Responsive';
 
 import { URLMap } from '../../../constants/urls';
-import { getPageInfo as getMainPageInfo } from '../../../services/purchased/main/selectors';
 
 export const TabMenuTypes = {
   ALL_BOOKS: 'ALL BOOKS',
@@ -21,46 +20,27 @@ const styles = {
   }),
 };
 
-class LNBTabBar extends React.Component {
-  get tabMenus() {
-    const { mainPageInfo } = this.props;
-    return [
-      {
-        type: TabMenuTypes.ALL_BOOKS,
-        name: '모든 책',
-        linkInfo: {
-          href: URLMap.main.href,
-          as: URLMap.main.as,
-          query: {
-            page: mainPageInfo.currentPage,
-            orderType: mainPageInfo.orderType,
-            orderBy: mainPageInfo.orderBy,
-            filter: mainPageInfo.filter,
-          },
-        },
-      },
-    ];
-  }
+const TabMenus = [
+  {
+    type: TabMenuTypes.ALL_BOOKS,
+    name: '모든 책',
+    linkInfo: {
+      href: URLMap.main.href,
+      as: URLMap.main.as,
+    },
+  },
+];
 
-  render() {
-    const { activeMenu } = this.props;
-    return (
-      <nav css={styles.LNBTabBarWrapper}>
-        <Responsive>
-          <TabBar>
-            {this.tabMenus.map(menu => (
-              <TabLinkItem name={menu.name} isActive={activeMenu === menu.type} {...menu.linkInfo} />
-            ))}
-          </TabBar>
-        </Responsive>
-      </nav>
-    );
-  }
-}
+const LNBTabBar = ({ activeMenu }) => (
+  <nav css={styles.LNBTabBarWrapper}>
+    <Responsive>
+      <TabBar>
+        {TabMenus.map(menu => (
+          <TabLinkItem name={menu.name} isActive={activeMenu === menu.type} {...menu.linkInfo} />
+        ))}
+      </TabBar>
+    </Responsive>
+  </nav>
+);
 
-const mapStateToProps = state => {
-  const mainPageInfo = getMainPageInfo(state);
-  return { mainPageInfo };
-};
-
-export default connect(mapStateToProps)(LNBTabBar);
+export default LNBTabBar;
