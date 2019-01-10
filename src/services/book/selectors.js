0c7556802f3f7b5a39b41a4c3bf1deee7201f8d0
김정근 <snorlax@ridi.com>
2019-01-10 20:31:36 +0900

    getUnit Selector 생성

--- src/services/book/selectors.js
+++ src/services/book/selectors.js
@@ -3,9 +3,17 @@ import { createSelector } from 'reselect';
 const getBookState = state => state.books;
 
 export const getBooks = (state, bookIds) =>
-  createSelector(getBookState, bookState =>
-    bookIds.reduce((previous, bookId) => {
-      previous[bookId] = bookState.books.get(bookId);
-      return previous;
-    }, {}),
+  createSelector(
+    getBookState,
+    bookState =>
+      bookIds.reduce((previous, bookId) => {
+        previous[bookId] = bookState.books.get(bookId);
+        return previous;
+      }, {}),
+  )(state);
+
+export const getUnit = (state, unitId) =>
+  createSelector(
+    getBookState,
+    bookState => bookState.units[unitId],
   )(state);
