7705d69b6a4b9266d564214fa34316c7b28b0882
김정근 <snorlax@ridi.com>
2019-01-11 17:26:54 +0900

    스켈레톤 컴포넌트들 스타일 수정

--- src/components/Skeleton/SkeletonUnitSection.jsx
+++ src/components/Skeleton/SkeletonUnitSection.jsx
@@ -1,15 +1,35 @@
-import { SkeletonUnitBook } from './components';
+import React from 'react';
+import classnames from 'classnames';
+import { css } from '@emotion/core';
+
+import { SkeletonUnitBook, SkeletonTextRow, SkeletonThinTextRow, SkeletonBoldTextRow } from './components';
+import { Responsive } from '../../styles/responsive';
+
+const styles = {
+  flexWrapper: css({
+    display: 'flex',
+    flexWrap: 'wrap',
+    flexDirection: 'row',
+    justifyContent: 'center',
+  }),
+
+  info: css({
+    ...Responsive.W1280({
+      justifyContent: 'left',
+    }),
+  }),
+};
 
 const SkeletonUnitSection = () => (
   <section>
-    <div>
+    <div css={classnames([styles.flexWrapper])}>
       <SkeletonUnitBook />
-      <span />
+      <SkeletonThinTextRow />
     </div>
-    <div>
-      <span />
-      <span />
-      <span />
+    <div css={classnames([styles.flexWrapper, styles.info])}>
+      <SkeletonBoldTextRow />
+      <SkeletonTextRow />
+      <SkeletonTextRow />
     </div>
   </section>
 );
--- src/components/Skeleton/components.jsx
+++ src/components/Skeleton/components.jsx
@@ -32,10 +32,42 @@ const styles = {
       height: 265,
     }),
   }),
+
+  thinTextRow: css({
+    width: 130,
+    height: 20,
+  }),
+
+  textRow: css({
+    height: 20,
+
+    ...Responsive.W834({
+      width: 400,
+    }),
+
+    ...Responsive.W1280({
+      width: 400,
+    }),
+  }),
+
+  boldTextRow: css({
+    width: 320,
+    height: 24,
+
+    ...Responsive.W834({
+      height: 28,
+    }),
+
+    ...Responsive.W1280({
+      height: 28,
+    }),
+  }),
 };
 
 export const SkeletonBook = () => <div css={classname([styles.skeleton, styles.book])} />;
 
 export const SkeletonUnitBook = () => <div css={classname([styles.skeleton, styles.unitBook])} />;
 
-export const SkeletonTextRow = () => <div css={classname([styles.skeleton])} />;
+export const SkeletonThinTextRow = () => <div css={classname([styles.skeleton, styles.thinTextRow])} />;
+export const SkeletonTextRow = () => <div css={classname([styles.skeleton, styles.textRow])} />;
+export const SkeletonBoldTextRow = () => <div css={classname([styles.skeleton, styles.boldTextRow])} />;
