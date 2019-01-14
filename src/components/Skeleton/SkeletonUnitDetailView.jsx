491d8631e8e61ce09c12c1fa3468cb03dc712924
김정근 <snorlax@ridi.com>
2019-01-14 22:02:35 +0900

    Skeleton Detail View 사이즈 정리

--- src/components/Skeleton/SkeletonUnitDetailView.jsx
+++ src/components/Skeleton/SkeletonUnitDetailView.jsx
@@ -1,27 +1,29 @@
 /** @jsx jsx */
 import { jsx, css } from '@emotion/core';
-
-import { SkeletonUnitBook, SkeletonTextRow, SkeletonThinTextRow, SkeletonBoldTextRow } from './components';
 import { Responsive } from '../../styles/responsive';
 
 const styles = {
-  section: css({
+  skeleton: css({
+    backgroundImage: 'linear-gradient(147deg, #f8f9fb, #f1f1f3 55%, #f8f9fb)',
+  }),
+
+  detailView: css({
     display: 'flex',
     flexDirection: 'column',
 
     marginTop: 28,
 
-    ...Responsive.W834({
-      marginTop: 50,
-    }),
-
-    ...Responsive.W1280({
+    ...Responsive.Pc({
       marginTop: 50,
       flexDirection: 'row',
     }),
+
+    ...Responsive.W1280({
+      marginLeft: 100,
+    }),
   }),
 
-  flexWrapper: css({
+  wrapper: css({
     display: 'flex',
     flexWrap: 'wrap',
     flexDirection: 'column',
@@ -29,39 +31,63 @@ const styles = {
     justifyContent: 'center',
   }),
 
-  info: css({
-    marginTop: 24,
+  thumbnailWrapper: css({}),
+  thumbnail: css({
+    width: 130,
+    height: 191,
 
     ...Responsive.Pc({
-      marginTop: 47,
+      width: 180,
+      height: 265,
     }),
+  }),
+  ridibooksLink: css({
+    marginTop: 16,
+    width: 130,
+    height: 20,
+  }),
 
-    ...Responsive.W1280({
+  infoWrapper: css({
+    marginTop: 24,
+
+    alignItems: 'start',
+    justifyContent: 'left',
+
+    ...Responsive.Pc({
+      marginTop: 48,
       marginLeft: 40,
-      alignItems: 'start',
-      justifyContent: 'left',
     }),
   }),
+  unitTitle: css({
+    width: 328,
+    height: 24,
 
-  margin16: css({
-    marginTop: 16,
+    ...Responsive.Pc({
+      width: 334,
+      height: 28,
+    }),
   }),
+  authorList: css({
+    width: 277,
+    height: 20,
 
-  margin24: css({
-    marginTop: 16,
+    ...Responsive.Pc({
+      marginTop: 16,
+      width: 400,
+      height: 20,
+    }),
   }),
 };
 
 const SkeletonUnitDetailView = () => (
-  <section css={styles.section}>
-    <div css={[styles.flexWrapper, styles.book]}>
-      <SkeletonUnitBook />
-      <SkeletonThinTextRow style={styles.margin16} />
+  <section css={styles.detailView}>
+    <div css={[styles.wrapper, styles.thumbnailWrapper]}>
+      <div css={[styles.skeleton, styles.thumbnail]} />
+      <div css={[styles.skeleton, styles.ridibooksLink]} />
     </div>
-    <div css={[styles.flexWrapper, styles.info]}>
-      <SkeletonBoldTextRow />
-      <SkeletonTextRow style={styles.margin16} />
-      <SkeletonTextRow style={styles.margin24} />
+    <div css={[styles.wrapper, styles.infoWrapper]}>
+      <div css={[styles.skeleton, styles.unitTitle]} />
+      <div css={[styles.skeleton, styles.authorList]} />
     </div>
   </section>
 );
