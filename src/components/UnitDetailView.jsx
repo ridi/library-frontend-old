45261fd6fee5baa919a8c71056a96f30d9ab5a76
김정근 <snorlax@ridi.com>
2019-01-14 21:07:33 +0900

    Detail View 내용 추가

--- src/components/UnitDetailView.jsx
+++ src/components/UnitDetailView.jsx
@@ -1,6 +1,8 @@
 /** @jsx jsx */
+import React from 'react';
 import { jsx, css } from '@emotion/core';
-import AuthorList from './AuthorList';
+
+import AuthorRole from '../constants/authorRole';
 
 const styles = {
   detailView: css({}),
@@ -10,21 +12,47 @@ const styles = {
   ridibooksLink: css({}),
 
   unitTitle: css({}),
+  authorList: css({}),
 };
 
-// 필요 데이터
-// Unit 데이터 (타이틀)
-// Unit의 대표 bookId
-// 대표 북의 데이터 (썸네일, 작가 등)
-const UnitDetailView = ({ unit, book }) => (
-  <section css={styles.detailView}>
-    <div css={styles.thumbnailWrapper}>
-      <img css={styles.thumbnail} src={book.thumbnails.large} alt={`${unit.title} 커버이미지`} />
-    </div>
-    <div css={styles.ridibooksLink}>리디북스로 가기</div>
-    <div css={styles.unitTitle}>{unit.title}</div>
-    <AuthorList authors={book.authors} />
-  </section>
-);
+class UnitDetailView extends React.Component {
+  compileAuthors() {
+    const {
+      book: { authors },
+    } = this.props;
+    const roles = AuthorRole.getPriorities(authors);
+
+    return roles
+      .reduce((previous, role) => {
+        const author = authors[role];
+
+        if (author) {
+          const names = author.map(value => value.name).join(',');
+          previous.push(`${names} ${AuthorRole.convertToString(role)}`);
+        }
+        return previous;
+      }, [])
+      .join(' | ');
+  }
+
+  render() {
+    // 필요 데이터
+    // Unit 데이터 (타이틀)
+    // Unit의 대표 bookId
+    // 대표 북의 데이터 (썸네일, 작가 등)
+    const { unit, book } = this.props;
+
+    return (
+      <section css={styles.detailView}>
+        <div css={styles.thumbnailWrapper}>
+          <img css={styles.thumbnail} src={book.thumbnail.large} alt={`${unit.title} 커버이미지`} />
+        </div>
+        <div css={styles.ridibooksLink}>리디북스로 가기</div>
+        <div css={styles.unitTitle}>{unit.title}</div>
+        <div css={styles.authorList}>{this.compileAuthors()}</div>
+      </section>
+    );
+  }
+}
 
 export default UnitDetailView;
