a1d22f222dd93f5bb61198f2188e6fc84c500661
김정근 <snorlax@ridi.com>
2019-01-14 18:35:03 +0900

    빈 스타일 추가

--- src/components/UnitDetailView.jsx
+++ src/components/UnitDetailView.jsx
@@ -2,21 +2,27 @@
 import { jsx, css } from '@emotion/core';
 import AuthorList from './AuthorList';
 
-const styles = {};
+const styles = {
+  detailView: css({}),
+
+  thumbnailWrapper: css({}),
+  thumbnail: css({}),
+  ridibooksLink: css({}),
+
+  unitTitle: css({}),
+};
 
 // 필요 데이터
 // Unit 데이터 (타이틀)
 // Unit의 대표 bookId
 // 대표 북의 데이터 (썸네일, 작가 등)
 const UnitDetailView = ({ unit, book }) => (
-  <section>
-    <div>
-      <div>
-        <img src={book.thumbnails.large} alt={`${unit.title} 커버이미지`} />
-      </div>
-      <div>리디북스로 가기</div>
+  <section css={styles.detailView}>
+    <div css={styles.thumbnailWrapper}>
+      <img css={styles.thumbnail} src={book.thumbnails.large} alt={`${unit.title} 커버이미지`} />
     </div>
-    <div>{unit.title}</div>
+    <div css={styles.ridibooksLink}>리디북스로 가기</div>
+    <div css={styles.unitTitle}>{unit.title}</div>
     <AuthorList authors={book.authors} />
   </section>
 );
