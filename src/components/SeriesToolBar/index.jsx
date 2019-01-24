54f5c8cad85d20c12556f0a29447c42a60b112db
Junheong Park <jh.park@ridi.com>
2019-01-24 23:55:09 +0900

    시리즈 뷰 툴바에 arrow 버튼 추가

--- src/components/SeriesToolBar/index.jsx
+++ src/components/SeriesToolBar/index.jsx
@@ -1,5 +1,7 @@
 /** @jsx jsx */
 import { jsx, css } from '@emotion/core';
+import { Icon } from '@ridi/rsg';
+import React from 'react';
 import Tool from '../Tool';
 import FlexBar from '../FlexBar';
 
@@ -8,6 +10,11 @@ const styles = {
     fontSize: 14,
     letterSpacing: -0.3,
     color: '#40474d',
+  }),
+  arrow: css({
+    marginLeft: 5,
+    width: 7,
+    height: 7,
   }),
 };
 
@@ -18,6 +25,7 @@ const SeriesToolBar = ({ orderTitle, toggleSortModal, toggleEditingMode }) => (
         {orderTitle ? (
           <button css={styles.orderButton} type="button" onClick={toggleSortModal}>
             {orderTitle}
+            <Icon name="arrow_1_down" css={styles.arrow} />
           </button>
         ) : null}
       </div>
