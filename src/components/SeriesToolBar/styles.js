7c1441df0c317c3778b2de888cb2fd17cc2fabb1
김정근 <snorlax@ridi.com>
2019-01-24 23:58:45 +0900

    SeriesToolBar 스타일링

--- src/components/SeriesToolBar/styles.js
+++ src/components/SeriesToolBar/styles.js
@@ -1,4 +1,6 @@
-export const seriesToolBar = {
+import { css } from '@emotion/core';
+
+export const seriesToolBar = css({
   boxSizing: 'border-box',
   borderTop: '1px solid #d1d5d9',
   borderBottom: '1px solid #d1d5d9',
@@ -7,4 +9,16 @@ export const seriesToolBar = {
   height: 46,
 
   backgroundColor: '#f3f4f5',
-};
+});
+
+export const orderButton = css({
+  fontSize: 14,
+  letterSpacing: -0.3,
+  color: '#40474d',
+});
+
+export const arrow = css({
+  marginLeft: 5,
+  width: 7,
+  height: 7,
+});
