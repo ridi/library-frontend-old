fd5dbe7926c31615a3e63a0ead35fcd95ec39353
nnyamm <nnyamm@ridi.com>
2019-01-24 17:30:02 +0900

    Separate styles

--- src/components/UnitDetailView/index.jsx
+++ src/components/UnitDetailView/index.jsx
@@ -1,195 +1,16 @@
-import { Icon } from '@ridi/rsg';
-
 /** @jsx jsx */
+import { jsx } from '@emotion/core';
+import { Icon } from '@ridi/rsg';
 import React from 'react';
-import { jsx, css } from '@emotion/core';
 import connect from 'react-redux/es/connect/connect';
 import config from '../../config';
-
 import AuthorRole from '../../constants/authorRole';
 import { UnitType } from '../../constants/unitType';
 import { BookFileType } from '../../services/book/constants';
 import { downloadBooks } from '../../services/common/actions';
-
-import { Responsive } from '../../styles/responsive';
 import { formatFileSize } from '../../utils/file';
 import { numberWithUnit } from '../../utils/number';
-
-const styles = {
-  detailView: css({
-    display: 'flex',
-    flexDirection: 'column',
-
-    marginTop: 28,
-
-    ...Responsive.Pc({
-      marginTop: 50,
-      flexDirection: 'row',
-    }),
-
-    ...Responsive.W1280({
-      marginLeft: 100,
-    }),
-  }),
-
-  wrapper: css({
-    display: 'flex',
-    flexWrap: 'wrap',
-    flexDirection: 'column',
-    alignItems: 'center',
-    justifyContent: 'center',
-  }),
-
-  thumbnailWrapper: css({}),
-  thumbnail: css({
-    backgroundImage: 'linear-gradient(to left, rgba(0, 0, 0, 0.2), rgba(0, 0, 0, 0.0) 6%, rgba(0, 0, 0, 0.0) 94%, rgba(0, 0, 0, 0.2))',
-    border: '1px solid rgba(0, 0, 0, 0.1)',
-    width: 130,
-
-    ...Responsive.Pc({
-      width: 180,
-    }),
-  }),
-  ridibooksLink: css({
-    fontSize: 15,
-    letterSpacing: -0.3,
-    color: '#1f8ce6',
-    marginTop: 16,
-  }),
-
-  infoWrapper: css({
-    marginTop: 24,
-
-    alignItems: 'start',
-    justifyContent: 'left',
-
-    ...Responsive.Pc({
-      marginTop: 48,
-      marginLeft: 40,
-    }),
-  }),
-  unitTitle: css({
-    fontSize: 20,
-    fontWeight: 'bold',
-    lineHeight: 1.54,
-    letterSpacing: -0.4,
-    color: '#212529',
-
-    ...Responsive.Pc({
-      fontSize: 30,
-      fontWeight: 'bold',
-      lineHeight: 1.03,
-      letterSpacing: -0.6,
-      color: '#212529',
-    }),
-  }),
-  authorList: css({
-    marginTop: 8,
-    fontSize: 15,
-    letterSpacing: -0.3,
-    color: '#40474d',
-
-    ...Responsive.Pc({
-      marginTop: 16,
-    }),
-  }),
-  bookDescription: css({
-    marginTop: 8,
-    fontSize: 15,
-    letterSpacing: -0.3,
-    color: '#40474d',
-    clear: 'both',
-
-    ...Responsive.Pc({
-      marginTop: 16,
-    }),
-  }),
-  bookDescriptionTitle: css({
-    fontWeight: 'bold',
-    letterSpacing: -0.28,
-    lineHeight: 'normal',
-  }),
-  bookDescriptionBody: css({
-    marginTop: 9,
-    lineHeight: 1.5,
-    display: '-webkit-box',
-    overflow: 'hidden',
-    textOverflow: 'ellipsis',
-    WebkitBoxOrient: 'vertical',
-  }),
-  bookDescriptionFolded: css({
-    // TODO: 변수화 해야 한다.
-    maxHeight: 9 * 23,
-    WebkitLineClamp: 9,
-  }),
-  bookDescriptionExpended: css({
-    // TODO: 변수화 해야 한다.
-    maxHeight: 'unset',
-    WebkitLineClamp: 'unset',
-  }),
-  bookDescriptionExpend: css({
-    textAlign: 'right',
-    marginTop: 11.5,
-  }),
-  bookDescriptionExpendButton: css({
-    fontSize: 15,
-    textAlign: 'right',
-    color: '#808991',
-    fill: '#9ea7ad',
-  }),
-  downloadButton: css({
-    width: '100%',
-    marginTop: 10,
-    marginBottom: 10,
-    height: 50,
-    borderRadius: 4,
-    boxShadow: '1px 1px 1px 0 rgba(31, 140, 230, 0.3)',
-    backgroundColor: '#1f8ce6',
-    fontSize: 16,
-    fontWeight: 'bold',
-    color: '#ffffff',
-    letterSpacing: -0.7,
-
-    ...Responsive.Pc({
-      width: 250,
-    }),
-  }),
-  drmFreeDownloadButton: css({
-    width: '100%',
-    marginBottom: 10,
-    height: 50,
-    borderRadius: 4,
-    boxShadow: '1px 1px 1px 0 rgba(209, 213, 217, 0.3)',
-    backgroundColor: '#ffffff',
-    fontSize: 16,
-    fontWeight: 'bold',
-    color: '#808991',
-    letterSpacing: -0.7,
-
-    ...Responsive.Pc({
-      width: 250,
-    }),
-  }),
-  fileInfo: css({
-    marginTop: 24,
-    marginBottom: 10,
-    fontSize: 15,
-    letterSpacing: -0.3,
-    color: '#808991',
-  }),
-  fileInfoText: css({
-    float: 'left',
-  }),
-  fileInfoDelimiter: css({
-    width: 1,
-    height: 9,
-    marginTop: 5,
-    marginLeft: 5,
-    marginRight: 5,
-    backgroundColor: '#d1d5d9',
-    float: 'left',
-  }),
-};
+import * as styles from './styles';
 
 class UnitDetailView extends React.Component {
   constructor(props) {
@@ -374,7 +195,7 @@ class UnitDetailView extends React.Component {
   }
 }
 
-const mapStateToProps = state => ({});
+const mapStateToProps = () => ({});
 
 const mapDispatchToProps = {
   dispatchDownloadBooks: downloadBooks,
