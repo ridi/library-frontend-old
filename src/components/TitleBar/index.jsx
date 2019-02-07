fdbcd105a0d437f121e85703dc28910388f99caa
Junheong Park <jh.park@ridi.com>
2019-02-07 18:25:33 +0900

    More 에서 옵션으로 버튼들 노출하도록 함

--- src/components/TitleBar/index.jsx
+++ src/components/TitleBar/index.jsx
@@ -1,6 +1,9 @@
 /** @jsx jsx */
 import { jsx } from '@emotion/core';
 import FlexBar from '../FlexBar';
+import * as styles from '../SearchBar/styles';
+import Editing from '../Tool/Editing';
+import More from '../Tool/More';
 import Title from './Title';
 
 const titleBar = {
@@ -10,10 +13,18 @@ const titleBar = {
   boxShadow: '0 2px 10px 0 rgba(0, 0, 0, .04)',
 };
 
-const TitleBar = ({ title, showCount, totalCount, href, as, query }) => (
+const TitleBar = ({ title, showCount, totalCount, href, as, query, showTools, toggleEditingMode }) => (
   <FlexBar
     css={titleBar}
     renderFlexLeft={() => <Title title={title} showCount={showCount} totalCount={totalCount} href={href} as={as} query={query} />}
+    renderFlexRight={() =>
+      showTools ? (
+        <div css={styles.toolsWrapper}>
+          {<Editing toggleEditingMode={toggleEditingMode} />}
+          {<More showViewType />}
+        </div>
+      ) : null
+    }
   />
 );
 
