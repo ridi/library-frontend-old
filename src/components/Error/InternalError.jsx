aa8c76a37014e15d82fc5a9003343b6a69eb7656
김정근 <snorlax@ridi.com>
2019-02-15 19:07:13 +0900

    공용 스타일 분리

--- src/components/Error/InternalError.jsx
+++ src/components/Error/InternalError.jsx
@@ -2,53 +2,7 @@
 import { jsx } from '@emotion/core';
 import Head from 'next/head';
 import ErrorBook from '../../svgs/ErrorBook.svg';
-
-const styles = {
-  pageError: {
-    textAlign: 'center',
-    padding: '60px 0',
-  },
-  errorTitle: {
-    margin: '8px 0',
-    fontSize: 60,
-    color: '#303538',
-  },
-  errorDescription: {
-    margin: '20px 0',
-    fontSize: 18,
-    lineHeight: '1.6em',
-    color: '#525A61',
-  },
-  errorButtonWrapper: {
-    display: 'inline-block',
-    marginTop: 36,
-  },
-  errorButton: {
-    display: 'inline-block',
-    width: 140,
-    padding: '14px 0',
-    margin: '0 2px',
-    fontSize: 16,
-    fontWeight: 700,
-    textAlign: 'center',
-    cursor: 'pointer',
-    transition: 'background .2s, color .2s',
-    outline: 'none',
-    boxSizing: 'border-box',
-    borderRadius: 4,
-    appearance: 'none',
-  },
-  whiteButton: {
-    color: '#808991',
-    background: '#fff',
-    border: '1px solid #d1d5d9',
-    boxShadow: '0 1px 1px 0 rgba(209,213,217,.3)',
-  },
-  icon: {
-    width: 94,
-    height: 79,
-  },
-};
+import * as styles from './styles';
 
 export const InternalError = ({ onClickHistoryBack }) => (
   <>
