35989baea43cb647b7bb3092aef30bed5d807723
김정근 <snorlax@ridi.com>
2019-02-15 21:17:54 +0900

    _error 버그 수정 (기본 버그 500으로 수정)

--- src/pages/_error.js
+++ src/pages/_error.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import { InternalError, NotFoundError } from '../components/Error';
 
+const Internal = 500;
 const NotFound = 404;
 
 export default class Error extends React.Component {
   static displayName = 'ErrorPage';
 
   static getInitialProps({ res, err }) {
-    let errorCode = NotFound;
+    let errorCode = Internal;
     if (res && res.statusCode) {
       errorCode = res.statusCode;
     } else if (err && err.statusCode) {
