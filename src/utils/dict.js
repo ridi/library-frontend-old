3c716fe5dc47bd238a4729dd232893cefc230ef7
김정근 <snorlax@ridi.com>
2019-02-15 20:14:39 +0900

    Dict에서 필요없는 값 지우는 유틸 추가

--- src/utils/dict.js
+++ src/utils/dict.js
@@ -1,1 +1,3 @@
 export const isEmpty = dict => Object.keys(dict).length === 0;
+
+export const simplifyValues = (dict, allowKeys) => allowKeys.reduce((previous, allowKey) => ({ ...previous, [allowKey]: dict[allowKey] }));
