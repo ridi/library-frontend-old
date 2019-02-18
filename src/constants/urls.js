b3d709359577699685205483e2d16d5a6f64baee
김정근 <snorlax@ridi.com>
2019-02-18 20:03:35 +0900

    Regex 로직 추가

--- src/constants/urls.js
+++ src/constants/urls.js
@@ -2,39 +2,65 @@ export const URLMap = {
   login: {
     href: '/login/',
     as: '/login/',
+    regex: /^\/login\/$/,
   },
   main: {
     href: '/purchased/main/',
     as: '/purchased/',
+    regex: /^\/purchased\/$/,
   },
   mainUnit: {
     href: '/purchased/mainUnit/',
     as: unitId => `/purchased/${unitId}/`,
+    regex: /^\/purchased\/(?<unitId>\d+)\/$/,
   },
   search: {
     href: '/purchased/search/',
     as: '/purchased/search/',
+    regex: /^\/purchased\/search\/$/,
   },
   searchUnit: {
     href: '/purchased/searchUnit/',
     as: unitId => `/purchased/search/${unitId}/`,
+    regex: /^\/purchased\/search\/(?<unitId>\d+)\/$/,
   },
   hidden: {
     href: '/purchased/hidden/',
     as: '/purchased/hidden/',
+    regex: /^\/purchased\/hidden\/$/,
   },
   hiddenUnit: {
     href: '/purchased/hiddenUnit/',
     as: unitId => `/purchased/hidden/${unitId}/`,
+    regex: /^\/purchased\/hidden\/(?<unitId>\d+)\/$/,
   },
   serialPreference: {
     href: '/serialPreference/',
     as: '/serial-preference/',
+    regex: /^\/serial-preference\/$/,
   },
-  parse: pathname => {
-    return {
-      href: URLMap.main.href,
-      as: URLMap.main.as,
-    };
-  },
+};
+
+export const toURLMap = pathname => {
+  const urlMaps = Object.keys(URLMap).map(key => URLMap[key]);
+  for (let index = 0; index < urlMaps.length; index += 1) {
+    const urlMap = urlMaps[index];
+    const result = urlMap.regex.exec(pathname);
+
+    if (result) {
+      if (typeof urlMap.as === 'function') {
+        const { unitId } = result.groups;
+        return {
+          href: { pathname: urlMap.href, query: { unitId } },
+          as: { pathname: urlMap.as(unitId) },
+        };
+      }
+      return urlMap;
+    }
+  }
+
+  return {
+    href: '/not-found/',
+    as: '/not-found/',
+  };
 };
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -5,14 +5,15 @@ import Router from 'next/router';
 
 import window, { LOCATION } from '../utils/window';
 import { makeLinkProps } from '../utils/uri';
-import { URLMap } from '../constants/urls';
+import { toURLMap } from '../constants/urls';
 
 export default class extends React.Component {
   componentDidMount() {
+    console.log('real?');
     const _location = window.get(LOCATION);
     const { pathname } = _location;
 
-    const { href, as } = URLMap.parse(pathname);
+    const { href, as } = toURLMap(pathname);
     const query = parse(_location.search, { charset: 'utf-8', ignoreQueryPrefix: true });
     const linkProps = makeLinkProps(href, as, query);
 
