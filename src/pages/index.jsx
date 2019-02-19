4d26d55a56240ddabb2761e5f0a93f35a1c89114
김정근 <snorlax@ridi.com>
2019-02-19 12:32:03 +0900

    페이지 Router 전환 시점 loadUserInfo로 이동

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,24 +1,7 @@
 import React from 'react';
-import { parse } from 'qs';
 import Head from 'next/head';
-import Router from 'next/router';
-
-import window, { LOCATION } from '../utils/window';
-import { makeLinkProps } from '../utils/uri';
-import { toURLMap } from '../constants/urls';
 
 export default class extends React.Component {
-  componentDidMount() {
-    const _location = window.get(LOCATION);
-    const { pathname } = _location;
-
-    const { href, as } = toURLMap(pathname);
-    const query = parse(_location.search, { charset: 'utf-8', ignoreQueryPrefix: true });
-    const linkProps = makeLinkProps(href, as, query);
-
-    Router.replace(linkProps.href, linkProps.as);
-  }
-
   render() {
     return (
       <>
--- src/services/account/sagas.js
+++ src/services/account/sagas.js
@@ -1,4 +1,5 @@
 import Router from 'next/router';
+import { parse } from 'qs';
 
 import { all, call, put, takeEvery, take, select } from 'redux-saga/effects';
 import { delay } from 'redux-saga';
@@ -7,12 +8,29 @@ import { LOAD_USER_INFO, START_ACCOUNT_TRACKER, setUserInfo } from './actions';
 import { fetchUserInfo } from './requests';
 
 import Window, { LOCATION } from '../../utils/window';
-import { URLMap } from '../../constants/urls';
+import { makeLinkProps } from '../../utils/uri';
+import { URLMap, toURLMap } from '../../constants/urls';
+
+function loadActualPage() {
+  const _location = Window.get(LOCATION);
+  const { pathname } = _location;
+
+  if (pathname === '/login/') {
+    Router.replace(URLMap.main.href, URLMap.main.as);
+    return;
+  }
+
+  const { href, as } = toURLMap(pathname);
+  const query = parse(_location.search, { charset: 'utf-8', ignoreQueryPrefix: true });
+  const linkProps = makeLinkProps(href, as, query);
+  Router.replace(linkProps.href, linkProps.as);
+}
 
 function* loadUserInfo() {
   try {
     const userInfo = yield call(fetchUserInfo);
     yield put(setUserInfo(userInfo));
+    loadActualPage();
   } catch (e) {
     Router.replace(URLMap.login.href, URLMap.login.as);
   }
