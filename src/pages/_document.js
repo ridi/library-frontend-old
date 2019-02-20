a88151c0c64cb9e9d02855baf2b5f34895bb2c4a
nnyamm <nnyamm@ridi.com>
2019-02-20 16:08:27 +0900

    Improve accessibility - Remove user scalable

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -25,7 +25,7 @@ export default class MyDocument extends Document {
         <Head>
           <meta httpEquiv="X-UA-Compatible" content="ie=edge" />
           <meta charSet="UTF-8" />
-          <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=0" />
+          <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0 />
           <Metadata />
           <Favicon />
 
