a6c94a068a2f1bf4e395f525c3b382f078f43e97
김정근 <snorlax@ridi.com>
2019-02-20 14:57:17 +0900

    index.html 캐시 컨트롤 수정

--- docs/script/upload.sh
+++ docs/script/upload.sh
@@ -2,4 +2,4 @@
 set -e
 
 aws s3 sync ./out $S3_PATH --cache-control max-age=31536000,s-maxage=31536000 --exclude index.html
-aws s3 sync ./out $INDEX_S3_PATH --cache-control max-age=31536000,s-maxage=31536000 --include index.html --exclude "*"
+aws s3 sync ./out $INDEX_S3_PATH --cache-control max-age=0,no-cache,no-store,must-revalidate --include index.html --exclude "*"
