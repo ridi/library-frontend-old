b702f7a08e1e1ffd6e29ebece894a616c1637211
김정근 <snorlax@ridi.com>
2019-02-27 19:04:37 +0900

    로그인 정보 로드 부분 단순 데이터 로딩으로 변경

--- src/services/account/actions.js
+++ src/services/account/actions.js
@@ -1,11 +1,6 @@
-export const LOAD_USER_INFO = 'LOAD_USER_INFO';
 export const SET_USER_INFO = 'SET_USER_INFO';
 
 export const START_ACCOUNT_TRACKER = 'START_ACCOUNT_TRACKER';
-
-export const loadUserInfo = () => ({
-  type: LOAD_USER_INFO,
-});
 
 export const setUserInfo = userInfo => ({
   type: SET_USER_INFO,
--- src/services/account/sagas.js
+++ src/services/account/sagas.js
@@ -1,55 +1,14 @@
-import Router from 'next/router';
-import { parse } from 'qs';
-
-import { all, call, put, takeEvery, take, select } from 'redux-saga/effects';
+import { all, call, put, take, select } from 'redux-saga/effects';
 import { delay } from 'redux-saga';
 
-import { LOAD_USER_INFO, START_ACCOUNT_TRACKER, setUserInfo } from './actions';
+import { START_ACCOUNT_TRACKER, setUserInfo } from './actions';
 import { fetchUserInfo } from './requests';
 
 import Window, { LOCATION } from '../../utils/window';
-import { makeLinkProps } from '../../utils/uri';
-import { URLMap, toURLMap } from '../../constants/urls';
 
-function loadActualPage() {
-  const _location = Window.get(LOCATION);
-  const { pathname } = _location;
-
-  const { href, as } = toURLMap(pathname);
-  const query = parse(_location.search, { charset: 'utf-8', ignoreQueryPrefix: true });
-  const linkProps = makeLinkProps(href, as, query);
-  Router.replace(linkProps.href, linkProps.as);
-}
-
-function* loadUserInfo() {
-  let userInfo;
-
-  // 로그인 페이지라면 먼저 UI 를 그려준다.
-  if (URLMap.login.regex.exec(Window.get(LOCATION).pathname)) {
-    yield delay(1);
-    loadActualPage();
-  }
-
-  // Step 1. 로그인이 되어 있는지 API 를 통해 확인하다.
-  try {
-    userInfo = yield call(fetchUserInfo);
-  } catch (e) {
-    // Step 2. 로그인 안되어 있다면 로그인 페이지 로드하고 종료
-    if (URLMap.login.regex.exec(Window.get(LOCATION).pathname)) {
-      loadActualPage();
-    }
-    return;
-  }
-
-  // Step 3. 로그인 되어 있는데 로그인 페이지에 있다면 모든 책으로 이동한다.
-  if (URLMap.login.regex.exec(Window.get(LOCATION).pathname)) {
-    Router.replace(URLMap.main.href, URLMap.main.as);
-    return;
-  }
-
-  // Stpe 4. 실제 페이지로 이동
+export function* loadUserInfo() {
+  const userInfo = yield call(fetchUserInfo);
   yield put(setUserInfo(userInfo));
-  loadActualPage();
 }
 
 function* accountTracker() {
@@ -74,5 +33,5 @@ function* accountTracker() {
 }
 
 export default function* accountRootSaga() {
-  yield all([accountTracker(), takeEvery(LOAD_USER_INFO, loadUserInfo)]);
+  yield all([accountTracker()]);
 }
