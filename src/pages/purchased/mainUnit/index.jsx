b1d7310ec9fed86836e1f8ba76c61acbae2dd104
김정근 <snorlax@ridi.com>
2019-03-12 21:22:59 +0900

    디버그용 콘솔 추가

--- src/pages/purchased/mainUnit/index.jsx
+++ src/pages/purchased/mainUnit/index.jsx
@@ -137,6 +137,9 @@ class MainUnit extends React.Component {
       ? OrderOptions.toSeriesList(bookUnitOfCount)
       : OrderOptions.toShelfList(bookUnitOfCount);
 
+    console.log('renderSeriesView');
+    console.log(items);
+    console.log(books);
     return (
       <SeriesView
         pageProps={{
