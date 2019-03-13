faa67fcd67165a78417e0a96e62ef3c99b312277
Junheong Park <jh.park@ridi.com>
2019-03-13 14:40:36 +0900

    purchased Url 을 books 로 변경, 도메인 root 접근시 리다이렉트 하지 않고 모든 책 보여주도록 함

--- server/routes.js
+++ server/routes.js
@@ -3,15 +3,15 @@ const nextRoutes = require('next-routes');
 const routes = nextRoutes();
 
 const APP_ROUTES = [
-  { page: 'index', pattern: '/' },
+  { page: 'purchased/main', pattern: '/' },
   { page: 'login', pattern: '/login' },
   { page: 'serialPreference', pattern: '/serial-preference' },
-  { page: 'purchased/main', pattern: '/purchased' },
-  { page: 'purchased/hidden', pattern: '/purchased/hidden' },
-  { page: 'purchased/hiddenUnit', pattern: '/purchased/hidden/:unit_id' },
-  { page: 'purchased/search', pattern: '/purchased/search' },
-  { page: 'purchased/searchUnit', pattern: '/purchased/search/:unit_id' },
-  { page: 'purchased/mainUnit', pattern: '/purchased/:unit_id' },
+  { page: 'purchased/main', pattern: '/books' },
+  { page: 'purchased/hidden', pattern: '/books/hidden' },
+  { page: 'purchased/hiddenUnit', pattern: '/books/hidden/:unit_id' },
+  { page: 'purchased/search', pattern: '/books/search' },
+  { page: 'purchased/searchUnit', pattern: '/books/search/:unit_id' },
+  { page: 'purchased/mainUnit', pattern: '/books/:unit_id' },
 ];
 
 APP_ROUTES.forEach(route => routes.add(route));
