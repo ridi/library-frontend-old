988270598dff7314d472827f080fb43f269e81ba
Junheong Park <jh.park@ridi.com>
2019-03-13 16:47:52 +0900

    Footer에서 Ridi Corp 문자열이 내려가지 않고 항상 줄바꿈 하도록 함

--- src/pages/base/Footer/index.jsx
+++ src/pages/base/Footer/index.jsx
@@ -11,34 +11,36 @@ const Footer = () => (
       <div css={styles.footerWrapper}>
         <div css={styles.footerTermWrapper}>
           <div css={styles.copyright}>© RIDI Corp.</div>
-          <ul css={styles.termsList}>
-            <li css={[styles.termsItem, styles.verticalSeparator]}>1644-0331</li>
-            <li css={[styles.termsItem, styles.verticalSeparator]}>
-              <a href="https://help.ridibooks.com/hc/ko/" target="_blank" rel="noopener noreferrer">
-                고객센터
-              </a>
-            </li>
-            <li css={[styles.termsItem, styles.verticalSeparator]}>
-              <a href="https://ridibooks.com/legal/terms" target="_blank" rel="noopener noreferrer">
-                이용약관
-              </a>
-            </li>
-            <li css={[styles.termsItem, styles.verticalSeparator]}>
-              <a href="https://ridibooks.com/legal/privacy" target="_blank" rel="noopener noreferrer">
-                <strong>개인 정보 처리 방침</strong>
-              </a>
-            </li>
-            <li css={[styles.termsItem, styles.verticalSeparator]}>
-              <a href="https://ridibooks.com/legal/youth" target="_blank" rel="noopener noreferrer">
-                청소년 보호 정책
-              </a>
-            </li>
-            <li css={[styles.termsItem]}>
-              <a href="http://ftc.go.kr/info/bizinfo/communicationList.jsp" target="_blank" rel="noopener noreferrer">
-                사업자 정보 확인
-              </a>
-            </li>
-          </ul>
+          <div>
+            <ul css={styles.termsList}>
+              <li css={[styles.termsItem, styles.verticalSeparator]}>1644-0331</li>
+              <li css={[styles.termsItem, styles.verticalSeparator]}>
+                <a href="https://help.ridibooks.com/hc/ko/" target="_blank" rel="noopener noreferrer">
+                  고객센터
+                </a>
+              </li>
+              <li css={[styles.termsItem, styles.verticalSeparator]}>
+                <a href="https://ridibooks.com/legal/terms" target="_blank" rel="noopener noreferrer">
+                  이용약관
+                </a>
+              </li>
+              <li css={[styles.termsItem, styles.verticalSeparator]}>
+                <a href="https://ridibooks.com/legal/privacy" target="_blank" rel="noopener noreferrer">
+                  <strong>개인 정보 처리 방침</strong>
+                </a>
+              </li>
+              <li css={[styles.termsItem, styles.verticalSeparator]}>
+                <a href="https://ridibooks.com/legal/youth" target="_blank" rel="noopener noreferrer">
+                  청소년 보호 정책
+                </a>
+              </li>
+              <li css={[styles.termsItem]}>
+                <a href="http://ftc.go.kr/info/bizinfo/communicationList.jsp" target="_blank" rel="noopener noreferrer">
+                  사업자 정보 확인
+                </a>
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
     </footer>
