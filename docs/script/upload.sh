175c09feadcc4185bff6ab172f20a1c1d2b9791b
Junheong Park <jh.park@ridi.com>
2019-03-13 15:28:21 +0900

    배포 버그 수정

--- docs/script/upload.sh
+++ docs/script/upload.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -e
 
-aws s3 sync ./out $S3_PATH --cache-control max-age=31536000,s-maxage=31536000 --exclude "index.jsx"
-aws s3 sync ./out $INDEX_S3_PATH --cache-control max-age=0,no-cache,no-store,must-revalidate --exclude "*" --include "index.jsx"
+aws s3 sync ./out $S3_PATH --cache-control max-age=31536000,s-maxage=31536000 --exclude "index.html"
+aws s3 sync ./out $INDEX_S3_PATH --cache-control max-age=0,no-cache,no-store,must-revalidate --exclude "*" --include "index.html"
