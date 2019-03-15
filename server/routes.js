c51d10860b000578793dda993973138bf50161a5
Junheong Park <jh.park@ridi.com>
2019-03-15 14:35:51 +0900

    선호작품 페이지 url 변경 - server

--- server/routes.js
+++ server/routes.js
@@ -5,7 +5,7 @@ const routes = nextRoutes();
 const APP_ROUTES = [
   { page: 'purchased/main', pattern: '/' },
   { page: 'login', pattern: '/login' },
-  { page: 'serialPreference', pattern: '/serial-preference' },
+  { page: 'serialPreferences', pattern: '/serial-preference' },
   { page: 'purchased/main', pattern: '/books' },
   { page: 'purchased/hidden', pattern: '/books/hidden' },
   { page: 'purchased/hiddenUnit', pattern: '/books/hidden/:unit_id' },
