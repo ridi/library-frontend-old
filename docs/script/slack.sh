125a01e59082dabe07365428699275fc927b3094
Junheong Park <jh.park@ridi.com>
2019-03-27 05:27:46 +0000

    Update slack.sh

--- docs/script/slack.sh
+++ docs/script/slack.sh
@@ -2,4 +2,4 @@
 set -e
 
 
-curl -X POST --data-urlencode "payload={\"text\": \"[웹 내 서재][$ENVIRONMENT - $TAG] 배포가 완료되었습니다.\nRepo: https://gitlab.com/ridicorp/account/library-web\"}" $SLACK_DEPLOY_HOOK
+curl -X POST --data-urlencode "payload={\"text\": \"[웹 내 서재][$ENVIRONMENT - $TAG] 배포가 완료되었습니다.\nRepo: https://gitlab.com/ridicorp/frontend/library-web\"}" $SLACK_DEPLOY_HOOK
