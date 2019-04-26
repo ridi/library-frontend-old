ef607cb6add80c8f61fdc41884929abc0ea376e8
Wonwoo Choi <chwo9843@gmail.com>
2019-04-26 02:40:53 +0000

    계정 정보가 넘어오면 센트리에 설정

--- src/services/router/routerWrapper.js
+++ src/services/router/routerWrapper.js
@@ -1,3 +1,4 @@
+import * as Sentry from '@sentry/browser';
 import React from 'react';
 import { connect } from 'react-redux';
 import Router from 'next/router';
@@ -10,18 +11,24 @@ const createConnectedRouterWrapper = () => {
   class RouterWrapper extends React.Component {
     componentDidMount() {
       Router.events.on('routeChangeStart', this.listenRouteChange);
-      Router.events.on('routeChangeError', this.finishRouteChange);
+      Router.events.on('routeChangeError', this.finishRouteChangeWithError);
       Router.events.on('routeChangeComplete', this.finishRouteChange);
     }
 
     componentWillUnmount() {
       Router.events.off('routeChangeStart', this.listenRouteChange);
-      Router.events.off('routeChangeError', this.finishRouteChange);
+      Router.events.off('routeChangeError', this.finishRouteChangeWithError);
       Router.events.off('routeChangeComplete', this.finishRouteChange);
     }
 
     listenRouteChange = as => {
       const location = locationFromUrl(as);
+      Sentry.addBreadcrumb({
+        category: 'router',
+        message: 'Route change initiated',
+        level: Sentry.Severity.Info,
+        data: { as, location: JSON.stringify(location) },
+      });
       const { setLocation: dispatchSetLocation } = this.props;
       dispatchSetLocation(location);
     };
@@ -33,6 +40,17 @@ const createConnectedRouterWrapper = () => {
       } else {
         this.commit();
       }
+    };
+
+    finishRouteChangeWithError = (err, url) => {
+      console.error(err);
+      Sentry.addBreadcrumb({
+        category: 'router',
+        message: 'Route change failed',
+        level: Sentry.Severity.Warning,
+        data: { url },
+      });
+      this.finishRouteChange();
     };
 
     checkMismatch() {
@@ -47,11 +65,22 @@ const createConnectedRouterWrapper = () => {
     }
 
     commit() {
+      Sentry.addBreadcrumb({
+        category: 'router',
+        message: 'Committing route',
+        level: Sentry.Severity.Info,
+      });
       const { commitLocation: dispatchCommitLocation } = this.props;
       dispatchCommitLocation();
     }
 
     rollback(url, as = url) {
+      Sentry.addBreadcrumb({
+        category: 'router',
+        message: 'Rolling back',
+        level: Sentry.Severity.Info,
+        data: { url },
+      });
       // Router의 Events를 보내지 않고 페이지 복구를 위해 Router코드를 직접 호출함
       // `change` 메소드의 간소화 버전, https://github.com/zeit/next.js/blob/canary/packages/next-server/lib/router/router.js
       const { rollbackLocation: dispatchRollbackLocation } = this.props;
