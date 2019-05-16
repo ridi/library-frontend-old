ed194eaf6bddf408d1c8ee21aeafe8ddbf8c17a9
Wonwoo Choi <chwo9843@gmail.com>
2019-05-16 10:00:32 +0000

    책장 리듀서 작성

---
+++ src/services/shelf/selectors.js
@@ -0,0 +1,70 @@
+import { createSelector } from 'reselect';
+import createCachedSelector from 're-reselect';
+
+/*
+ * getShelves(
+ *   state: State,
+ *   options: {
+ *     orderBy: string;
+ *     orderDirection: string;
+ *     page: number;
+ *   },
+ * ): { loading: boolean; items: Array<string> | null }
+ *
+ * loading: false, items: null이면 로드 필요
+ * */
+export const getShelves = createSelector(
+  state => state.shelf.shelves,
+  (_, { orderBy }) => orderBy,
+  (_, { orderDirection }) => orderDirection,
+  (_, { page }) => page,
+  (shelves, orderBy, orderDirection, page) => shelves[`${orderBy}_${orderDirection}_${page}`] || { loading: false, items: null },
+);
+
+// getShelfCount(state: State): number | null
+// 로드 중이면 null
+export const getShelfCount = state => state.shelf.shelfCount;
+
+// getShelfName(state: State, uuid: string): string | null
+// 로드 중이거나 책장이 없으면 null
+export const getShelfName = createCachedSelector(
+  state => state.shelf.shelf,
+  (_, uuid) => uuid,
+  (shelf, uuid) => (shelf[uuid] == null ? null : shelf[uuid].name),
+)((_, uuid) => uuid);
+
+// getShelfBookCount(state: State, uuid: string): number | null
+// 로드 중이거나 책장이 없으면 null
+export const getShelfBookCount = createCachedSelector(
+  state => state.shelf.shelf,
+  (_, uuid) => uuid,
+  (shelf, uuid) => (shelf[uuid] == null ? null : shelf[uuid].bookCount),
+)((_, uuid) => uuid);
+
+/*
+ * getShelfBooks(
+ *   state: State,
+ *   uuid: string,
+ *   options: {
+ *     orderBy: string;
+ *     orderDirection: string;
+ *     page: number;
+ *   },
+ * ): { loading: boolean; items: Array<string> | null }
+ *
+ * loading: false, items: null이면 로드 필요
+ * */
+export const getShelfBooks = createCachedSelector(
+  state => state.shelf.shelf,
+  (_, uuid) => uuid,
+  (_, __, { orderBy }) => orderBy,
+  (_, __, { orderDirection }) => orderDirection,
+  (_, __, { page }) => page,
+  (shelf, uuid, orderBy, orderDirection, page) => {
+    const target = shelf[uuid];
+    if (target == null) {
+      return { loading: false, items: null };
+    }
+    return target.books[`${orderBy}_${orderDirection}_${page}`] || { loading: false, items: null };
+  },
+)((_, uuid) => uuid);
