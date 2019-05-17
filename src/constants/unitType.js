4a394ddbbcbcc3319b71a9a64d52f37933c9ea2e
nnyamm <nnyamm@ridi.com>
2019-05-17 12:50:09 +0900

    주석 추가

--- src/constants/unitType.js
+++ src/constants/unitType.js
@@ -20,6 +20,10 @@ export class UnitType {
   }
 
   static isCollection(unitType) {
+    /** 사내에서 커뮤니케이션시 '콜렉션 도서' 라고 부르는 도서들의 type 이
+     * dev 에서는 'shelf', 'collection' 이 섞여있고 production 에서는 'shelf' 만 넘어옴.
+     * 19.05.17 현재 테섭에서도 'shelf' 만 넘겨 주도록 계정팀에서 보정 중
+     * TODO : 보정 완료 후 COLLECTION 제거 및 주석 수정 */
     return unitType === this.SHELF || unitType === this.COLLECTION;
   }
 
