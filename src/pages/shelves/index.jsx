99b31bb27e7ab64f78b8d06fdaa2a77b945b2042
nnyamm <nnyamm@ridi.com>
2019-05-22 15:12:00 +0900

    Add default structure

---
+++ src/pages/shelves/index.jsx
@@ -0,0 +1,35 @@
+/** @jsx jsx */
+import { jsx } from '@emotion/core';
+import Head from 'next/head';
+import React from 'react';
+import { connect } from 'react-redux';
+import { TabBar, TabMenuTypes } from '../base/LNB';
+import ShelvesWrapper from '../../components/ShelvesWrapper';
+
+class Shelves extends React.Component {
+  render() {
+    return (
+      <>
+        <Head>
+          <title>책장 목록 - 내 서재</title>
+        </Head>
+        <TabBar activeMenu={TabMenuTypes.SHELVES} />
+        <ShelvesWrapper />
+      </>
+    );
+  }
+}
+
+const mapStateToProps = state => {
+  console.log(state);
+  return {};
+};
+
+const mapDispatchToProps = {
+  // dispatchSelectAllBooks: selectAllBooks,
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps,
+)(Shelves);
