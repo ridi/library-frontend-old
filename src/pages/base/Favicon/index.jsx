a30264bd5e024393d60d32f6cfb9c6c396742241
Wonwoo Choi <wonwoo.choi@ridi.com>
2019-06-25 15:17:53 +0900

    새 파비콘 적용

--- src/pages/base/Favicon/index.jsx
+++ src/pages/base/Favicon/index.jsx
@@ -8,7 +8,7 @@ const Favicon = () => (
     <link rel="apple-touch-icon" sizes="180x180" href={`${config.STATIC_URL}/static/favicon/apple-touch-icon.png`} />
     <link rel="icon" type="image/png" sizes="32x32" href={`${config.STATIC_URL}/static/favicon/favicon-32x32.png`} />
     <link rel="icon" type="image/png" sizes="16x16" href={`${config.STATIC_URL}/static/favicon/favicon-16x16.png`} />
-    <link rel="manifest" href={`${config.STATIC_URL}/static/favicon/site.2019-02-25.webmanifest.json`} />
+    <link rel="manifest" href={`${config.STATIC_URL}/static/favicon/site.2019-06-03.webmanifest.json`} />
     <link rel="mask-icon" href={`${config.STATIC_URL}/static/favicon/safari-pinned-tab.svg`} color="#5bbad5" />
     <meta name="msapplication-TileColor" content="#da532c" />
     <meta name="theme-color" content="#ffffff" />
