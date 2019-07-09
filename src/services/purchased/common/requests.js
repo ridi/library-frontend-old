01bb392d747bf885fcfd55864e46d871b501c98a
Wonwoo Choi <wonwoo.choi@ridi.com>
2019-07-09 11:12:16 +0900

    사용하는 파일만 이동 후 정리

--- src/services/purchased/common/requests.js
+++ src/services/purchased/common/requests.js
@@ -1,16 +1,10 @@
-import { call, put } from 'redux-saga/effects';
+import { put } from 'redux-saga/effects';
 import { getAPI } from '../../../api/actions';
 import { HttpStatusCode } from '../../../api/constants';
 
 import config from '../../../config';
 import { makeURI } from '../../../utils/uri';
-import { fetchLibraryBookData } from '../../book/requests';
 import { NotFoundReadLatestError } from './errors';
-
-// TODO: compat
-export function* fetchItems(bookIds) {
-  return yield call(fetchLibraryBookData, bookIds);
-}
 
 export function* fetchReadLatestBookId(seriesId) {
   const api = yield put(getAPI());
