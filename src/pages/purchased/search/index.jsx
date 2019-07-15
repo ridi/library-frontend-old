75ee9a8b1ed47a77319912d3866668314ba691eb
Wonwoo Choi <wonwoo.choi@ridi.com>
2019-07-15 14:19:04 +0900

    페이지 범위 밖이면 가장 가까운 페이지로 리다이렉트하는 컴포넌트 작성 (#94)

--- src/pages/purchased/search/index.jsx
+++ src/pages/purchased/search/index.jsx
@@ -3,13 +3,14 @@ import { jsx } from '@emotion/core';
 import React from 'react';
 import Helmet from 'react-helmet';
 import { connect } from 'react-redux';
-import { Link, Redirect } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import { ButtonType } from '../../../components/ActionBar/constants';
 import BookDownLoader from '../../../components/BookDownLoader';
 import { Books } from '../../../components/Books';
 import Editable from '../../../components/Editable';
 import Empty from '../../../components/Empty';
 import { BookError } from '../../../components/Error';
+import PageRedirect from '../../../components/PageRedirect';
 import ResponsivePaginator from '../../../components/ResponsivePaginator';
 import SearchBar from '../../../components/SearchBar';
 import SelectShelfModal from '../../../components/SelectShelfModal';
@@ -238,28 +239,13 @@ function Search(props) {
     title = '검색 - 내 서재';
   }
 
-  let redirection = null;
-  if (totalPages > 0) {
-    const realPage = Math.max(1, Math.min(totalPages, currentPage));
-    if (currentPage !== realPage) {
-      const newUrlParams = new URLSearchParams(location.search);
-      newUrlParams.set('page', realPage);
-      const newSearch = newUrlParams.toString();
-      const to = {
-        ...location,
-        search: newSearch !== '' ? `?${newSearch}` : '',
-      };
-      redirection = <Redirect to={to} />;
-    }
-  }
-
   if (showShelves) {
     return (
       <>
         <Helmet>
           <title>{title}</title>
         </Helmet>
-        {redirection}
+        <PageRedirect currentPage={currentPage} totalPages={totalPages} />
         <SelectShelfModal onBackClick={handleShelfBackClick} onShelfSelect={handleShelfSelect} />
       </>
     );
@@ -270,7 +256,7 @@ function Search(props) {
       <Helmet>
         <title>{title}</title>
       </Helmet>
-      {redirection}
+      <PageRedirect currentPage={currentPage} totalPages={totalPages} />
       <TabBar activeMenu={TabMenuTypes.ALL_BOOKS} />
       <Editable
         allowFixed
