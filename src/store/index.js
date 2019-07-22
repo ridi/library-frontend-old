ab315d53758252797cdf8cdf71bef677afeb42e7
nnyamm <nnyamm@ridi.com>
2019-07-22 18:41:42 +0900

    실수로 제거한 Redux devtools 관련 코드 복구

--- src/store/index.js
+++ src/store/index.js
@@ -6,11 +6,13 @@ import rootReducer from './reducers';
 import rootSaga from './sagas';
 
 export const makeStore = (initialState, context) => {
+  const devTools = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__;
+  const composeEnhancers = devTools || compose;
   const apiMiddleware = createApiMiddleware(context);
   const sagaMiddleware = createSagaMiddleware();
   const middlewares = [apiMiddleware, sagaMiddleware];
 
-  const store = createStore(rootReducer, initialState, compose(applyMiddleware(...middlewares)));
+  const store = createStore(rootReducer, initialState, composeEnhancers(applyMiddleware(...middlewares)));
 
   store.runSagaTask = () => {
     store.sagaTask = sagaMiddleware.run(rootSaga);
