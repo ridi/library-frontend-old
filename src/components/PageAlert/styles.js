c2e76a072d19662e44d6abbad80a496e47ded0ca
nnyamm <nnyamm@ridi.com>
2019-07-30 20:18:24 +0900

    책장 선택 화면에 베타 안내 추가 및 스타일 수정

--- src/components/PageAlert/styles.js
+++ src/components/PageAlert/styles.js
@@ -3,19 +3,20 @@ import { css } from '@emotion/core';
 export const wrapper = css`
   border-radius: 4px;
   background-color: #e6e8eb;
-  padding: 13px 16px 13px 40px;
-  position: relative;
+  padding: 13px 16px;
+  display: flex;
   width: 100%;
   box-sizing: border-box;
 `;
 
+const iconSize = 16;
+
 export const icon = css`
-  width: 16px;
-  height: 16px;
+  width: ${iconSize}px;
+  height: ${iconSize}px;
   fill: #9ea7ad;
-  position: absolute;
-  top: 13px;
-  left: 16px;
+  margin-right: 8px;
+  flex: 0 0 auto;
 `;
 
 export const message = css`
