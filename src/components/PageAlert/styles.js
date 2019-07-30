8f1356674cce11c1e1eb15c1c86c6e948ab55449
nnyamm <nnyamm@ridi.com>
2019-07-30 14:48:43 +0900

    내책장에 Beta alert 추가

---
+++ src/components/PageAlert/styles.js
@@ -0,0 +1,25 @@
+import { css } from '@emotion/core';
+
+export const wrapper = css`
+  border-radius: 4px;
+  background-color: #e6e8eb;
+  padding: 13px 16px 13px 40px;
+  position: relative;
+  width: 100%;
+  box-sizing: border-box;
+`;
+
+export const icon = css`
+  width: 16px;
+  height: 16px;
+  fill: #9ea7ad;
+  position: absolute;
+  top: 13px;
+  left: 16px;
+`;
+
+export const message = css`
+  line-height: 1.38em;
+  color: #525a61;
+  font-size: 13px;
+`;
