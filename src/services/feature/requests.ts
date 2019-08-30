0b3be6d4be85e725fa1668373d9419d914070d2c
Wonwoo Choi <wonwoo.choi@ridi.com>
2019-08-30 18:18:22 +0900

    feature, shelf requests에 Runtypes 적용

--- src/services/feature/requests.ts
+++ src/services/feature/requests.ts
@@ -1,9 +1,16 @@
+import * as R from 'runtypes';
+
 import { getApi } from '../../api';
 import config from '../../config';
 import { makeURI } from '../../utils/uri';
 
+const RFeatureCheckResponse = R.Record({
+  is_tester: R.Boolean,
+});
+
 export async function fetchIsFeatureEnabled(featureId) {
   const api = getApi();
   const response = await api.get(makeURI(`/tests/u/${featureId}/check/`, {}, config.LIBRARY_API_BASE_URL));
-  return response.data.is_tester;
+  const data = RFeatureCheckResponse.check(response.data);
+  return data.is_tester;
 }
