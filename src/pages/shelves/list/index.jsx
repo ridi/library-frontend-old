46a050c8626416e4f72b1283ce12849f356b195a
nnyamm <nnyamm@ridi.com>
2019-09-26 18:34:20 +0900

    더이상 사용하지 않는 툴팁 요소 제거

--- src/pages/shelves/list/index.jsx
+++ src/pages/shelves/list/index.jsx
@@ -13,7 +13,6 @@ import { Shelves } from '../../../components/Shelves';
 import { SkeletonShelves } from '../../../components/Skeleton/SkeletonShelves';
 import { Editing, ShelfOrder } from '../../../components/Tool';
 import { Add } from '../../../components/Tool/Add';
-import { Tooltip } from '../../../components/Tooltip';
 import { OrderOptions } from '../../../constants/orderOptions';
 import { SHELVES_LIMIT_PER_PAGE } from '../../../constants/page';
 import { SHELF_NAME_LIMIT, SHELVES_LIMIT } from '../../../constants/shelves';
@@ -159,11 +158,7 @@ const ShelvesList = props => {
     const order = OrderOptions.toKey(orderBy, orderDirection);
     return (
       <div css={styles.toolsWrapper}>
-        <Add onClickAddButton={handleAddShelf}>
-          <Tooltip name="SHELVES_TOOLTIP" expires={new Date(2019, 8, 20)}>
-            책장을 만들어 원하는 책을 담아보세요!
-          </Tooltip>
-        </Add>
+        <Add onClickAddButton={handleAddShelf} />
         <Editing toggleEditingMode={toggleSelectMode} />
         <ShelfOrder
           order={order}
