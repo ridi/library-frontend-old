d47d3e8c71ee31d9f5ff9053826f02785ee3900d
Wonwoo Choi <wonwoo.choi@ridi.com>
2019-10-21 11:52:44 +0900

    검색에 취소 버튼 추가

--- src/pages/shelves/detail/SearchModal/SearchBar.jsx
+++ src/pages/shelves/detail/SearchModal/SearchBar.jsx
@@ -14,11 +14,33 @@ const searchBarStyles = {
     box-shadow: 0 1px 6px 0 rgba(0, 0, 0, 0.04);
     background-color: #f3f4f5;
   `,
+  cancelButton: css`
+    width: 55px;
+    height: 30px;
+    margin-left: 12px;
+    border: 1px solid #d1d5d9;
+    border-radius: 4px;
+    line-height: 28px;
+    font-size: 13px;
+    font-weight: bold;
+    text-align: center;
+    color: #808991;
+    background-color: white;
+  `,
 };
 
-export default function SearchBar({ filter, keyword, onClear, onConfirm, onFilterChange, onKeywordChange }) {
+export default function SearchBar({ filter, isSearching, keyword, onClear, onConfirm, onFilterChange, onKeywordChange }) {
   const filterOptions = useSelector(filterSelectors.getFilterOptions);
   const left = <SearchBox allowEmptySearch keyword={keyword} onClear={onClear} onSubmit={onConfirm} onKeywordChange={onKeywordChange} />;
-  const right = <Tools.Filter filter={filter} filterOptions={filterOptions} onFilterChange={onFilterChange} />;
+  const right = (
+    <>
+      <Tools.Filter filter={filter} filterOptions={filterOptions} onFilterChange={onFilterChange} />
+      {isSearching && (
+        <button type="button" css={searchBarStyles.cancelButton} onClick={onClear}>
+          취소
+        </button>
+      )}
+    </>
+  );
   return <FlexBar css={searchBarStyles.bar} flexLeft={left} flexRight={right} />;
 }
