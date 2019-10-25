a8a4d2f64c4320ad6883a6cccaf16e2807dab89d
Wonwoo Choi <wonwoo.choi@ridi.com>
2019-10-25 15:40:10 +0900

    사용하지 않는 파일 제거

--- src/utils/dict.js
+++
@@ -1,3 +0,0 @@
-export const isEmpty = dict => Object.keys(dict).length === 0;
-
-export const simplifyValues = (dict, allowKeys) => allowKeys.reduce((previous, allowKey) => ({ ...previous, [allowKey]: dict[allowKey] }));
