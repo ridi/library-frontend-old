7dc7b0fd51760a2385cd30c812b96423bf574613
nnyamm <nnyamm@ridi.com>
2019-11-27 19:09:34 +0900

    store 수정

--- src/services/shelf/selectors.js
+++ src/services/shelf/selectors.js
@@ -84,7 +84,7 @@ export const getShelfAllBookIds = createCachedSelector(
   state => state.shelf,
   (_, uuid) => uuid,
   (shelf, uuid) => {
-    const allItem = shelf.shelf[uuid]?.books?.all?.items;
+    const allItem = shelf.shelf[uuid]?.books?.allItem;
     return allItem && allItem.length > 0 ? allItem.map(item => shelf.itemMap[item]?.bookIds[0]) : null;
   },
 )((_, uuid) => uuid);
