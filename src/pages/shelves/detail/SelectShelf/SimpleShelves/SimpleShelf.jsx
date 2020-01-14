125bfb77bfed3954bec55a59cf6c17fcf700d15d
nnyamm <nnyamm@ridi.com>
2020-01-14 18:28:43 +0900

    SimpleShelf 의 선택영역 레이아웃 수정 및 비활성화 되어있는 경우 접근성 코드 개선

--- src/pages/shelves/detail/SelectShelf/SimpleShelves/SimpleShelf.jsx
+++ src/pages/shelves/detail/SelectShelf/SimpleShelves/SimpleShelf.jsx
@@ -1,42 +1,58 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 
 import * as selectionSelectors from 'services/selection/selectors';
 import { ITEMS_LIMIT_PER_SHELF } from 'constants/shelves';
 import * as shelfSelectors from 'services/shelf/selectors';
+import * as toastActions from 'services/toast/actions';
+import { ToastStyle } from 'services/toast/constants';
 import CheckCircle from 'svgs/CheckCircle.svg';
 
-import DisabledButton from './DisabledButton';
 import * as styles from './styles';
 
 const SimpleShelf = ({ shelfId, isSelect, handleShelfSelectChange }) => {
+  const dispatch = useDispatch();
   const shelfName = useSelector(state => shelfSelectors.getShelfName(state, shelfId));
   const shelfBookCount = useSelector(state => shelfSelectors.getShelfBookCount(state, shelfId));
   const selectedBookCount = useSelector(selectionSelectors.getTotalSelectedCount);
   const disabled = shelfBookCount + selectedBookCount >= ITEMS_LIMIT_PER_SHELF;
+  const buttonLabel = disabled ? '선택 불가 책장' : `${shelfName} 선택`;
+  const handleListClick = () => {
+    if (disabled) {
+      dispatch(
+        toastActions.showToast({
+          message: `최대 ${ITEMS_LIMIT_PER_SHELF}권까지 추가할 수 있습니다.`,
+          toastStyle: ToastStyle.BLUE,
+          withBottomFixedButton: true,
+        }),
+      );
+    }
+  };
 
   return (
     <div css={[styles.simpleShelf, disabled && styles.disabledShelf]}>
+      <label css={styles.radioButtonLabel} onClick={handleListClick}>
+        <input
+          type="radio"
+          name="simpleShelves"
+          css={styles.invisibleRadioInput}
+          onChange={() => {
+            handleShelfSelectChange(shelfId);
+          }}
+          disabled={disabled}
+        />
+        <span className="a11y">{buttonLabel}</span>
+      </label>
       <div css={styles.shelfCheckIconWrapper}>
-        <label css={[styles.checkButton, disabled && styles.disabledCheckButton]}>
+        <div css={[styles.checkButton, disabled && styles.disabledCheckButton]}>
           <span css={[styles.checkIconBorder, isSelect && styles.checked]} />
           <CheckCircle css={styles.checkIcon} />
-          <input
-            type="radio"
-            name="simpleShelves"
-            css={styles.invisibleRadioInput}
-            onChange={() => {
-              handleShelfSelectChange(shelfId);
-            }}
-            disabled={disabled}
-          />
-        </label>
+        </div>
       </div>
       <div css={styles.shelfMetaWrapper}>
         <p css={styles.shelfName}>{shelfName}</p>
         <p css={styles.shelfBookCount}>{shelfBookCount > 0 ? shelfBookCount : ''}</p>
       </div>
-      {disabled && <DisabledButton />}
     </div>
   );
 };
